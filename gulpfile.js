5f2c23cddcabfa0633439303c4104463f6546282
Erick Merchant <erickmerchant@gmail.com>
2014-07-29 00:23:03 -0700

    Remove unused packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var gulp = require('gulp');
-var chalk = require('chalk');
 var gulputil = require('gulp-util');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
