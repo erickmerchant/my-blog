2b77d556da71527ebced74bbfaa209ca7a8d3a5c
Erick Merchant <erickmerchant@gmail.com>
2014-08-16 01:11:54 -0700

    New theme

--- tasks/watch.js
+++ tasks/watch.js
@@ -7,18 +7,18 @@ gulp.task('watch', ['default'], function () {
 
     gulp.watch('base/**', ['base']);
     gulp.watch('content/uploads/*.jpg', ['images']);
-    gulp.watch('assets/js/**.js', ['js']);
+    gulp.watch('assets/js/**/**.js', ['js']);
     gulp.watch('assets/geomicons/**/**.svg', ['geomicons']);
 
     if (!argh.argv.dev) {
 
-        gulp.watch('assets/scss/**.scss', ['scss', 'uncss']);
+        gulp.watch('assets/scss/**/**.scss', ['scss', 'uncss']);
         gulp.watch('templates/**/**.html', ['site', 'htmlmin', 'uncss']);
         gulp.watch('content/**', ['site', 'htmlmin', 'uncss']);
 
     } else {
 
-        gulp.watch('assets/scss/**.scss', ['scss']);
+        gulp.watch('assets/scss/**/**.scss', ['scss']);
         gulp.watch('templates/**/**.html', ['site']);
         gulp.watch('content/**', ['site']);
     }
