1ae9d4b086aa490282882641a8229ef07392f66a
Erick Merchant <erickmerchant@gmail.com>
2014-10-05 22:25:18 -0700

    Experiment with rework-inherit

--- tasks/css.js
+++ tasks/css.js
@@ -7,14 +7,27 @@ var uncss = require('gulp-uncss');
 var minifycss = require('gulp-minify-css');
 var tap = require('gulp-tap');
 var glob = require('glob');
-var basswork = require('gulp-basswork');
+var rework = require('gulp-rework');
 var css_files = require('./settings.json').css_files;
 var concat = require('gulp-concat');
+var calc = require('rework-calc');
+var media = require('rework-custom-media');
+var npm = require('rework-npm');
+var vars = require('rework-vars');
+var colors = require('rework-plugin-colors');
+var inherit = require('rework-inherit');
 
 gulp.task('css', function () {
 
     var stream = gulp.src(css_files)
-        .pipe(basswork())
+        .pipe(rework(
+            npm(),
+            vars(),
+            media(),
+            calc,
+            colors(),
+            inherit()
+        ))
         .pipe(autoprefixer('> 1%', 'last 2 versions'))
         .pipe(concat("site.css"))
         .pipe(gulp.dest('site/assets'));
