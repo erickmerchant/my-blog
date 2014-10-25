2d48a7dbe672d09727926905f1882e704eb324c9
Erick Merchant <erickmerchant@gmail.com>
2014-10-25 13:57:23 -0700

    Move assets

--- tasks/watch.js
+++ tasks/watch.js
@@ -7,19 +7,19 @@ gulp.task('watch', ['default'], function () {
 
     gulp.watch('base/**', ['base']);
     gulp.watch('content/uploads/*.jpg', ['images', 'images-minify']);
-    gulp.watch('assets/js/**/**.js', ['js']);
+    gulp.watch('js/**/**.js', ['js']);
     gulp.watch('bower_components/prism/components/**/**.js', ['js']);
     gulp.watch('bower_components/geomicons-open/**/**.svg', ['icons-append']);
 
     if (!argh.argv.dev) {
 
-        gulp.watch('assets/css/**/**.css', ['css-minify']);
+        gulp.watch('css/**/**.css', ['css-minify']);
         gulp.watch('templates/**/**.html', ['html-minify', 'css-minify']);
         gulp.watch('content/**', ['html-minify', 'css-minify']);
 
     } else {
 
-        gulp.watch('assets/css/**/**.css', ['css']);
+        gulp.watch('css/**/**.css', ['css']);
         gulp.watch('templates/**/**.html', ['html']);
         gulp.watch('content/**', ['html']);
     }
