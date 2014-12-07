118c63e8841185d60e6b203d37c451c7d356e035
Erick Merchant <erickmerchant@gmail.com>
2014-12-07 01:15:38 -0800

    Upgrade static-engine things

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var tasks = require('gulp-tasks');
 var argv = require('argh').argv;
 var build_directory = argv.dev ? "./staging/" : './site/';
+var Promise = require('es6-promise').Promise;
 
 tasks.config({
     directory: build_directory,
@@ -27,7 +28,7 @@ tasks.config({
     },
     build: function() {
 
-        var nunjucks = require('nunjucks');
+        var nunjucks = require('static-engine-renderer-nunjucks');
         var engine = require('static-engine');
         var content = require('static-engine-content');
         var defaults = require('static-engine-defaults');
@@ -37,41 +38,52 @@ tasks.config({
         var marked = require('static-engine-converter-marked');
         var file = require('static-engine-converter-file');
         var frontmatter = require('static-engine-converter-frontmatter');
-        var site = engine(build_directory, nunjucks.render);
-
-        content.configure('./content/', [
-            file(),
-            frontmatter(),
-            marked()
-        ]);
+        var render = require('static-engine-render');
+        var sort = require('static-engine-sort');
+        var posts;
+        var defaults;
+        var formula = [];
 
         nunjucks.configure('./templates/', {
             autoescape: true
         });
 
-        site.route('index.html')
-            .use(content('posts/*'))
-            .use(pager())
-            .use(first())
-            .render('post.html');
+        render.configure(build_directory);
 
-        site.route('posts/{slug}/index.html')
-            .use(content('posts/*'))
-            .use(pager())
-            .render('post.html');
+        content.configure('./content/', [
+            file,
+            frontmatter,
+            marked()
+        ]);
 
-        site.route('posts/index.html')
-            .use(content('posts.md'))
-            .use(collection('posts', content('posts/*')))
-            .render('posts.html');
+        posts = content('posts/*');
 
-        site.route('404.html')
-            .use(content('404.md'))
-            .render('404.html');
+        defaults = defaults('./content/defaults.yml');
 
-        site.after(defaults('./content/defaults.yml'));
+        formula.push([
+            posts,
+            sort.date,
+            pager,
+            defaults,
+            render('posts/{slug}/index.html', nunjucks('post.html')),
+            first,
+            render('index.html', nunjucks('post.html'))
+        ]);
 
-        return site.build();
+        formula.push([
+            content('posts.md'),
+            collection('posts', [posts, sort.date]),
+            defaults,
+            render('posts/index.html', nunjucks('posts.html'))
+        ]);
+
+        formula.push([
+            content('404.md'),
+            defaults,
+            render('404.html', nunjucks('404.html'))
+        ]);
+
+        return engine(formula);
     }
 });
 
