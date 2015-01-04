0df0cabff07858954c6c62873ebad65d3408cc86
Erick Merchant <erickmerchant@gmail.com>
2015-01-03 22:25:34 -0800

    Use centralized server

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var config = {
         thumbnails: [626]
     }
 };
+var serve = require('server');
 
 function build() {
 
@@ -209,77 +210,6 @@ function images() {
     return merged;
 }
 
-function serve() {
-
-    var node_static = require('node-static');
-    var chalk = require('chalk');
-    var moment = require('moment');
-    var fs = require('fs');
-
-    var server = new node_static.Server(config.directory);
-
-    require('http').createServer(function (request, response) {
-        request.addListener('end', function () {
-            server.serve(request, response, function (err, result) {
-
-                var color;
-                var status;
-                var end = true;
-
-                if (err) {
-
-                    if (err.status === 404 && fs.existsSync(config.directory + '404.html')) {
-
-                        end = false;
-
-                        server.serveFile('404.html', 404, {},
-                        request, response);
-
-                    } else {
-                        response.writeHead(err.status, err.headers);
-                    }
-
-                    status = err.status;
-
-                } else {
-                    status = response.statusCode;
-                }
-
-                switch (true) {
-                    case status >= 500:
-                    color = chalk.red;
-                    break;
-
-                    case status >= 400:
-                    color = chalk.yellow;
-                    break;
-
-                    case status >= 300:
-                    color = chalk.cyan;
-                    break;
-
-                    case status >= 200:
-                    color = chalk.green;
-                    break;
-
-                    case status >= 100:
-                    color = chalk.magenta;
-                    break;
-                }
-
-                console.log('[' + chalk.gray(moment().format('HH:mm:ss')) + '] [' + color(status) + '] ' + color(request.url));
-
-                if (end) {
-
-                    response.end();
-                }
-            });
-
-        }).resume();
-
-    }).listen(8080);
-}
-
 gulp.task('default', gulp.parallel(base, gulp.series(icons, build, html, css), images));
 
 gulp.task('watch', function() {
@@ -290,4 +220,4 @@ gulp.task('watch', function() {
     gulp.watch(['templates/**/**.html', 'content/**'], gulp.series(build, html, css));
 });
 
-gulp.task('serve', gulp.parallel('default', 'watch', serve));
+gulp.task('serve', gulp.parallel('default', 'watch', serve(config.directory)));
