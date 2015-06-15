aa7740dae1dcc55b8d776e800f593f93609f0c34
Erick Merchant <erickmerchant@gmail.com>
2015-06-14 20:30:39 -0700

    Change bach usage

--- cms.js
+++ cms.js
@@ -5,23 +5,37 @@
 const vinylFS = require('vinyl-fs')
 const sergeant = require('sergeant')
 const chalk = require('chalk')
-const bach = require('./lib/extensions/bach.js')
-const base = require('./lib/tasks/base.js')
-const pages = require('./lib/tasks/pages.js')
-const icons = require('./lib/tasks/icons.js')
-const css = require('./lib/tasks/css.js')
-const gitStatus = require('./lib/tasks/git-status.js')
-const minifyHTML = require('./lib/tasks/minify-html.js')
-const images = require('./lib/tasks/images.js')
-const serve = require('./lib/tasks/serve.js')
-const make = require('./lib/tasks/make.js')
-const move = require('./lib/tasks/move.js')
-const allParallel = bach.parallel(base, bach.series(pages, icons, minifyHTML, css), images)
+const bach = require('bach')
+const pretty = require('pretty-hrtime')
+const extensions = {
+  create: function (fn) {
+    return { name: fn.name || 'anonymous' }
+  },
+  before: function (storage) {
+    storage.started = process.hrtime()
+
+    console.log(chalk.magenta(storage.name) + ' starting ... ')
+  },
+  after: function (result, storage) {
+    console.log(chalk.magenta(storage.name) + ' finished in ' + chalk.cyan(pretty(process.hrtime(storage.started))))
+  }
+}
+const base = require('./tasks/base.js')
+const pages = require('./tasks/pages.js')
+const icons = require('./tasks/icons.js')
+const css = require('./tasks/css.js')
+const gitStatus = require('./tasks/git-status.js')
+const minifyHTML = require('./tasks/minify-html.js')
+const images = require('./tasks/images.js')
+const serve = require('./tasks/serve.js')
+const make = require('./tasks/make.js')
+const move = require('./tasks/move.js')
+const allParallel = bach.parallel(base, bach.series(pages, icons, minifyHTML, css), images, extensions)
 const app = sergeant('CMS for erickmerchant.com')
 
-app.command('update', { description: 'Build the site once' }, bach.series(allParallel, gitStatus))
+app.command('update', { description: 'Build the site once' }, bach.series(allParallel, gitStatus, extensions))
 
-app.command('watch', { description: 'Build the site then watch for changes. Run a server' }, bach.parallel(allParallel, watch, serve))
+app.command('watch', { description: 'Build the site then watch for changes. Run a server' }, bach.parallel(allParallel, watch, serve, extensions))
 
 app.command('make', {
   description: 'Make new content',
@@ -58,5 +72,5 @@ app.run(function (err, result) {
 function watch () {
   vinylFS.watch('base/**/*', base)
   vinylFS.watch('content/uploads/**/*.jpg', images)
-  vinylFS.watch(['css/**/*.css', 'templates/**/*.html', 'content/**/*.md'], bach.series(pages, icons, minifyHTML, css))
+  vinylFS.watch(['css/**/*.css', 'templates/**/*.html', 'content/**/*.md'], bach.series(pages, icons, minifyHTML, css, extensions))
 }
