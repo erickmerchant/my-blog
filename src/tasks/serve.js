01d62694c68de11e3e5ac4ba3c6f6446c7fac518
Erick Merchant <erickmerchant@gmail.com>
2015-07-09 22:36:49 -0700

    Find port

--- src/tasks/serve.js
+++ src/tasks/serve.js
@@ -5,29 +5,36 @@ const directory = require('./directory.js')
 const express = require('express')
 const _static = require('express-static')
 const logger = require('express-log')
+const portfinder = require('portfinder')
 
 module.exports = function serve (done) {
-  const app = express()
+  portfinder.getPort(function (err, port) {
+    if (err) {
+      done(err)
+    } else {
+      const app = express()
 
-  app.use(logger())
+      app.use(logger())
 
-  app.use(_static(directory))
+      app.use(_static(directory))
 
-  app.use(function (req, res, next) {
-    res.status(404)
+      app.use(function (req, res, next) {
+        res.status(404)
 
-    if (req.accepts('html')) {
-      res.sendFile(path.resolve(directory, '404.html'), {}, function (err) {
-        if (err) {
-          res.type('txt').send('Not found')
+        if (req.accepts('html')) {
+          res.sendFile(path.resolve(directory, '404.html'), {}, function (err) {
+            if (err) {
+              res.type('txt').send('Not found')
+            }
+          })
         }
       })
+
+      app.listen(port, function () {
+        console.log('server is running at %s', port)
+      })
+
+      done()
     }
   })
-
-  app.listen(8088, function () {
-    console.log('server is running at %s', this.address().port)
-  })
-
-  done()
 }
