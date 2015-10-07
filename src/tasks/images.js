5fe1a7afe73aca534f7c565702a08da30ea17c4b
Erick Merchant <erickmerchant@gmail.com>
2015-10-06 22:02:52 -0700

    Switch to sharp to resize images

--- src/tasks/images.js
+++ src/tasks/images.js
@@ -3,31 +3,34 @@
 const path = require('path')
 const vinylFS = require('vinyl-fs')
 const directory = require('./directory.js')
-const imageresize = require('gulp-image-resize')
+const sharp = require('sharp')
 const imagemin = require('gulp-imagemin')
+const glob = require('glob')
+const mkdirp = require('mkdirp')
 
 module.exports = function images () {
-  return Promise.all([
-    new Promise(function (resolve, reject) {
-      vinylFS.src('content/uploads/*.jpg')
+  return (new Promise(function (resolve, reject) {
+    mkdirp(path.join(directory, 'uploads/thumbnails/'), function (err) {
+      if (err) throw err
+
+      glob('content/uploads/*.jpg', function (err, files) {
+        if (err) throw err
+
+        resolve(Promise.all(files.map(function (file) {
+          return sharp(file).resize(622).toFile(path.join(directory, 'uploads/thumbnails/', path.basename(file)))
+        })))
+      })
+    })
+  }))
+  .then(function () {
+    return new Promise(function (resolve, reject) {
+      vinylFS.src(['content/uploads/**/*.jpg', 'content/uploads/*.jpg'])
         .pipe(imagemin({
           progressive: true
         }))
         .pipe(vinylFS.dest(path.join(directory, 'uploads')))
         .once('end', resolve)
         .on('error', reject)
-    }),
-    new Promise(function (resolve, reject) {
-      vinylFS.src('content/uploads/*.jpg')
-        .pipe(imageresize({
-          width: 622
-        }))
-        .pipe(imagemin({
-          progressive: true
-        }))
-        .pipe(vinylFS.dest(path.join(directory, 'uploads/thumbnails/')))
-        .once('end', resolve)
-        .on('error', reject)
     })
-  ])
+  })
 }
