7beba048c9910f9ec92da8a732148cf70dae6b8e
Erick Merchant <erickmerchant@gmail.com>
2016-02-15 21:25:23 -0800

    New Atlatl

--- tasks/pages.js
+++ tasks/pages.js
@@ -40,10 +40,10 @@ function pages () {
 
     done(null, pages)
   }
-  const templates = atlatl('./templates/')
+  const templates = atlatl()
   const renderer = function (name) {
     return function (page, done) {
-      templates(name)
+      templates('./templates/' + name)
       .then(function (template) {
         done(null, template(page))
       })
