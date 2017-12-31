97f6142df14050e3fa1a4d8b008d557a1aae067c
Erick Merchant <erickmerchant@gmail.com>
2017-12-31 02:10:05 -0800

    Upgrade html

--- html.js
+++ html.js
@@ -1,212 +1,135 @@
-const slug = require('slug')
-const moment = require('moment-timezone')
-const groupby = require('lodash.groupby')
 const icons = require('geomicons-open')
 const host = 'http://erickmerchant.com'
 
-module.exports = function ({collection}) {
-  collection('post', function ({on, save, remove, read}) {
-    on('draft', function ({parameter, option}) {
-      option('title', {
-        description: 'the title',
-        required: true
-      })
+module.exports = function ({html, safe, route, link, content}) {
+  return html`
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>${route(function (on) {
+        on('/404.html', '404 Not Found')
 
-      option('summary', {
-        description: 'the summary',
-        default: { value: '' }
-      })
+        on('/posts/', 'Posts')
 
-      return function (args) {
-        save('posts/:slug', {
-          title: args.title,
-          summary: args.summary,
-          slug: slug(args.title.toLowerCase()),
-          timeZone: moment.tz.guess()
-        })
-      }
-    })
-
-    on('publish', function ({parameter, option}) {
-      parameter('post', {
-        description: 'the post to publish',
-        required: true
-      })
-
-      return function (args) {
-        read(args.post, 'posts/:slug', function (post) {
-          save(
-            'posts/:time.:slug',
-            Object.assign(post, {
-              time: Date.now()
-            }),
-            function () {
-              remove(args.post)
-            }
-          )
-        })
-      }
-    })
-  })
-
-  return function ({get, html, save, safe, link, dev}) {
-    save('404', layout({
-      title: '404 Not Found',
-      url: '/404.html',
-      main: ({title, url}) => html`
-      <div>
-        <h1>${title}</h1>
-        <p>That page doesn't exist. It was either moved, removed, or never existed.</p>
-      </div>`
-    }))
-
-    const routes = ['posts/:time.:slug']
-
-    if (dev) {
-      routes.push('posts/:slug')
-    }
-
-    get(routes, function (posts) {
-      posts = posts.reverse().map(function (post) {
-        if (post.time) {
-          post.date = moment(new Date(Number(post.time))).tz(post.timeZone)
-        } else {
-          post.date = moment()
-        }
-
-        return post
-      })
-
-      const grouped = groupby(posts, (post) => post.date.format('MMMM YYYY'))
-
-      save('posts/', layout({
-        title: 'Posts',
-        url: '/posts/',
-        main: ({title, url}) => html`
-        <h1>${title}</h1>
-        ${safe(Object.keys(grouped).map((monthYear) => html`
-          <section>
-            <h2>
-              ${icon('calendar')}
-              ${monthYear}
-            </h2>
-            <dl>
-              ${safe(grouped[monthYear].map((post) => html`
-                <dt><a href="${link('/posts/:slug/', post)}">${post.title}</a></dt>
-                <dd>${post.summary}</dd>`
-              ))}
-            </dl>
-          </section>`
-        ))}`
-      }))
-
-      if (posts.length > 0) {
-        posts.forEach(function (post, index) {
-          let url = link('/posts/:slug/', post)
+        content.forEach(function (post, index) {
+          const title = `${post.title} | Posts`
 
           if (index === 0) {
-            url = ['/', url]
+            on('/', title)
           }
 
-          save(url, layout({
-            title: `${post.title} | Posts`,
-            url: link('/posts/:slug/', post),
-            main: ({title, url}) => html`
-            <article>
-              <header>
-                <h1>${post.title}</h1>
-                <p>
-                  <time class="bold" datetime="${post.date.format('YYYY-MM-DD')}">
-                    ${icon('calendar')}
-                    ${post.date.format('MMMM D, YYYY')}
-                  </time>
-                </p>
-              </header>
-              <div>${safe(post.content)}</div>
-            </article>
-            <nav class="flex row justify-around padding-2 bold">
-              ${safe(
-              posts[index + 1]
-              ? html`
-                <a class="align-left nowrap border-radius padding-2 background-blue white" rel="prev" href="${link('/posts/:slug/', posts[index + 1])}">
-                  ${icon('chevronLeft')}
-                  Older
-                </a>`
-              : html`
-                <span class="align-left nowrap border-radius padding-2 background-gray white is-disabled">
-                  ${icon('chevronLeft')}
-                  Older
-                </span>`
-              )}
-              ${safe(
-              posts[index - 1]
-              ? html`
-                <a class="align-right nowrap border-radius padding-2 background-blue white" rel="next" href="${link('/posts/:slug/', posts[index - 1])}">
-                  Newer
-                  ${icon('chevronRight')}
-                </a>`
-              : html`
-                <span class="align-right nowrap border-radius padding-2 background-gray white is-disabled">
-                  Newer
-                  ${icon('chevronRight')}
-                </span>`
-              )}
-            </nav>`
-          }))
+          on(link('/posts/:slug/', post), title)
         })
-      }
-    })
-
-    function layout ({title, url, main}) {
-      return html`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="utf-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-          <title>${title}</title>
-          <link href="/favicon.png" rel="shortcut icon" type="image/png">
-          <link href="/bundle.css" rel="stylesheet" type="text/css">
-          <link rel="canonical" href="${host}${url}">
-        </head>
-        <body class="flex column">
-          <nav class="background-black align-center bold mobile-padding-2">
-            <div class="padding-2">
-              <span class="margin-1">
-                <a class="white" href="/">Erick Merchant</a>
-              </span>
-              <span class="margin-1">
-                <a class="white" href="/posts/">
-                  ${icon('calendar')} Posts
-                </a>
-              </span>
-              <span class="margin-1">
-                <a class="white" href="http://github.com/erickmerchant/">
-                  ${icon('github')} GitHub
-                </a>
-              </span>
-            </div>
-          </nav>
-          <main class="auto padding-2 max-width full-width margin-horizontal-auto" role="main">
-            ${safe(main({title, url}))}
-          </main>
-          <footer class="background-light-gray font-size-small padding-2 align-center bold" role="contentinfo">
-            <a class="margin-1 inline-block" href="https://github.com/erickmerchant/erickmerchant.com-source">
-              ${icon('github')}
-              View Source
+      })}</title>
+      <link href="/favicon.png" rel="shortcut icon" type="image/png">
+      <link href="/bundle.css" rel="stylesheet" type="text/css">
+      <link rel="canonical" href="${host}${route()}">
+    </head>
+    <body class="flex column">
+      <nav class="background-black align-center bold mobile-padding-2">
+        <div class="padding-2">
+          <span class="margin-1">
+            <a class="white" href="/">Erick Merchant</a>
+          </span>
+          <span class="margin-1">
+            <a class="white" href="/posts/">
+              ${icon('calendar')} Posts
             </a>
-            <span class="margin-1 inline-block">&copy; Erick Merchant, ${(new Date()).getFullYear()}</span>
-          </footer>
-        </body>
-      </html>`
-    }
+          </span>
+          <span class="margin-1">
+            <a class="white" href="http://github.com/erickmerchant/">
+              ${icon('github')} GitHub
+            </a>
+          </span>
+        </div>
+      </nav>
+      <main class="auto padding-2 max-width full-width margin-horizontal-auto" role="main">
+        ${route(function (on) {
+          on('/404.html', html`
+            <h1>404 Not Found</h1>
+            <p>That page doesn't exist. It was either moved, removed, or never existed.</p>
+          `)
 
-    function icon (name) {
-      return safe(html`
+          on('/posts/', html`
+            <h1>Posts</h1>
+            <dl>
+              ${content.map((post) => html`
+                <dt><a href="${link('/posts/:slug/', post)}">${post.title}</a></dt>
+                <dd>${post.summary}</dd>
+              `)}
+            </dl>
+          `)
+
+          content.forEach(function (post, index) {
+            const body = html`
+              <article>
+                <header>
+                  <h1>${post.title}</h1>
+                  <p>
+                    <time class="bold" datetime="${post.date.toISOString()}">
+                      ${icon('calendar')}
+                      ${post.date.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})}
+                    </time>
+                  </p>
+                </header>
+                <div>${safe(post.content)}</div>
+              </article>
+              <nav class="flex row justify-around padding-2 bold">
+                ${
+                content[index + 1]
+                ? html`
+                  <a class="align-left nowrap border-radius padding-2 background-blue white" rel="prev" href="${link('/posts/:slug/', content[index + 1])}">
+                    ${icon('chevronLeft')}
+                    Older
+                  </a>`
+                : html`
+                  <span class="align-left nowrap border-radius padding-2 background-gray white is-disabled">
+                    ${icon('chevronLeft')}
+                    Older
+                  </span>`
+                }
+                ${
+                content[index - 1]
+                ? html`
+                  <a class="align-right nowrap border-radius padding-2 background-blue white" rel="next" href="${link('/posts/:slug/', content[index - 1])}">
+                    Newer
+                    ${icon('chevronRight')}
+                  </a>`
+                : html`
+                  <span class="align-right nowrap border-radius padding-2 background-gray white is-disabled">
+                    Newer
+                    ${icon('chevronRight')}
+                  </span>`
+                }
+              </nav>
+            `
+
+            if (index === 0) {
+              on('/', body)
+            }
+
+            on(link('/posts/:slug/', post), body)
+          })
+        })}
+      </main>
+      <footer class="background-light-gray font-size-small padding-2 align-center bold" role="contentinfo">
+        <a class="margin-1 inline-block" href="https://github.com/erickmerchant/erickmerchant.com-source">
+          ${icon('github')}
+          View Source
+        </a>
+        <span class="margin-1 inline-block">&copy; Erick Merchant, ${(new Date()).getFullYear()}</span>
+      </footer>
+    </body>
+  </html>`
+
+  function icon (name) {
+    return html`
       <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
         <path d="${icons.paths[name]}" />
-      </svg>`)
-    }
+      </svg>
+    `
   }
 }
