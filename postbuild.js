6165319337b4cfdf7992cfcbad600918cee970af
Erick Merchant <erickmerchant@gmail.com>
2019-01-20 12:37:35 -0800

    More headers

--- postbuild.js
+++ postbuild.js
@@ -6,7 +6,7 @@ const globby = require('globby')
 
 ;(async () => {
   const files = await globby('./dist/**/*.{css,mjs}')
-  const headers = []
+  const headers = ['  Link: </content/posts/index.json>; rel=preload; as=fetch']
 
   for (const file of files) {
     const relative = path.relative('./dist', file)
@@ -26,6 +26,15 @@ const globby = require('globby')
 
   for (const path of ['/', '/posts/*']) {
     lines.push(path, ...headers)
+  }
+
+  const posts = await globby('./dist/content/posts/*.md')
+
+  for (const post of posts) {
+    const relative = path.relative('./dist', post)
+    const slug = path.basename(post, '.md')
+
+    lines.push(`/posts/${slug}`, `  Link: </${relative}>; rel=preload; as=fetch`)
   }
 
   await writeFile('./dist/_headers', lines.join('\n'))
