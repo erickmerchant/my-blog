689876a408cd2c7e4881fbe06424edb4f0c04487
Erick Merchant <erickmerchant@gmail.com>
2019-05-12 19:39:28 -0700

    Update

--- src/js/body.mjs
+++ src/js/body.mjs
@@ -4,7 +4,7 @@ import {html, safe} from '@erickmerchant/framework'
 import {route, link} from './router.mjs'
 import {dispatchLocation} from './store.mjs'
 
-const {body, nav, div, a, main, h1, p, footer, span, article, header, time} = html
+const {site, article, pagination, enabledAnchor, disabledAnchor, heading1, paragraph} = html
 
 export default (state, commit) => {
   const getOnClick = (href) => (e) => {
@@ -15,81 +15,63 @@ export default (state, commit) => {
     dispatchLocation(commit, href)
   }
 
-  return body(
-    {
-      onappend() {
-        window.onpopstate = () => {
-          dispatchLocation(commit, document.location.pathname)
-        }
-
+  return site`<body
+    onappend=${() => {
+      window.onpopstate = () => {
         dispatchLocation(commit, document.location.pathname)
       }
-    },
-    nav(
-      {class: 'nav'},
-      div(
-        {},
-        a({
-          href: '/',
-          onclick: getOnClick('/')
-        }, 'Erick Merchant'),
-        a({
-          href: 'https://github.com/erickmerchant'
-        }, 'Projects')
-      )
-    ),
-    main({}, ...route(state.location, (on) => {
+
+      dispatchLocation(commit, document.location.pathname)
+    }}
+  >
+    <nav class="nav">
+      <div>
+        <a
+          href="/"
+          onclick=${getOnClick('/')}
+        >Erick Merchant</a>
+        <a href="https://github.com/erickmerchant">Projects</a>
+      </div>
+    </nav>
+    <main>${route(state.location, (on) => {
       on(['/posts/:slug/', '/'], () => [
-        article(
-          {
-            class: 'article'
-          },
-          header(
-            {},
-            h1({}, state.post.title),
-            time(
-              {
-                datetime: (new Date(state.post.date)).toISOString()
-              },
-              (new Date(state.post.date)).toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})
-            )
-          ),
-          safe(state.post.html)
-        ),
-        nav(
-          {class: 'pagination'},
-          Boolean(state.prev)
-            ? a({
+        article`<article class="article">
+          <header>
+            <h1>${state.post.title}</h1>
+            <time datetime=${(new Date(state.post.date)).toISOString()}>
+              ${(new Date(state.post.date)).toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})}
+            </time>
+          </header>
+          ${safe(state.post.html)}
+        </article>`,
+        pagination`<nav class="pagination">
+          ${Boolean(state.prev)
+            ? enabledAnchor`<a ${{
               class: 'prev',
               rel: 'prev',
               href: link('/posts/:slug/', state.prev),
               onclick: getOnClick(link('/posts/:slug/', state.prev))
-            }, 'Older')
-            : span({class: 'prev'}, 'Older'),
-          Boolean(state.next)
-            ? a({
+            }}>${'Older'}<a>`
+            : disabledAnchor`<span class=${'prev'}>${'Older'}</span>`}
+          ${Boolean(state.next)
+            ? enabledAnchor`<a ${{
               class: 'next',
               rel: 'next',
               href: link('/posts/:slug/', state.next),
               onclick: getOnClick(link('/posts/:slug/', state.next))
-            }, 'Newer')
-            : span({class: 'next'}, 'Newer')
-        )
+            }}>${'Newer'}<a>`
+            : disabledAnchor`<span class=${'next'}>${'Newer'}</span>`}
+        </nav>`
       ])
 
       on(() => [
-        h1({}, state.title),
-        p({}, state.error != null ? state.error.message : '')
+        heading1`<h1>${state.title}</h1>`,
+        paragraph`<p>${state.error != null ? state.error.message : ''}</p>`
       ])
-    })),
-    footer(
-      {
-        class: 'footer'
-      },
-      a({
-        href: 'https://github.com/erickmerchant/my-blog'
-      }, 'View Source'),
-      span({}, `© ${(new Date()).getFullYear()} Erick Merchant`)
-    )
-  )
+    })}</main>
+    <footer class="footer">
+      <a href="https://github.com/erickmerchant/my-blog">View Source</a>
+      <span>${`© ${(new Date()).getFullYear()} Erick Merchant`}</span>
+    </footer>
+  </body>`
 }
