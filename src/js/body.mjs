5a89be3b74c0dfcb22edc0c58493d692af52a538
Erick Merchant <erickmerchant@gmail.com>
2019-08-22 22:16:56 -0400

    Make it green

--- src/js/body.mjs
+++ src/js/body.mjs
@@ -2,7 +2,7 @@ import {view, safe} from '@erickmerchant/framework'
 import {route, link} from './router.mjs'
 import {dispatchLocation} from './store.mjs'
 
-const {site, main, liAnchor, error} = view
+const {site, article, liAnchor, error} = view
 
 export default ({state, commit, next}) => {
   const anchorAttrs = (href) => {
@@ -23,48 +23,48 @@ export default ({state, commit, next}) => {
   })
 
   return site`<body>
-    <nav class="nav">
-      <ul>
-        <li><a ${anchorAttrs('/')}>Erick Merchant</a></li>
-        <li><a href="https://github.com/erickmerchant">Projects</a></li>
-      </ul>
-    </nav>
-    ${route(state.location, (on) => {
-      on('/posts/:slug/', () => main`<main>
-        <article class="article">
+    <header>
+      <nav class="nav">
+        <ul>
+          <li><a ${anchorAttrs('/')}>Erick Merchant</a></li>
+          <li><a href="https://github.com/erickmerchant">Projects</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      ${route(state.location, (on) => {
+        on('/posts/:slug/', () => article`<article>
           <header>
             <h1>${state.post.title}</h1>
             <time datetime=${(new Date(state.post.date)).toISOString()}>
               ${(new Date(state.post.date)).toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})}
             </time>
           </header>
-          ${safe(state.post.html)}
-        </article>
-        <nav class="pagination">
-          <ul>
-            ${Boolean(state.prev)
-              ? liAnchor`<li><a ${{
-                class: 'prev',
-                rel: 'prev',
-                ...anchorAttrs(link('/posts/:slug/', state.prev))
-              }}>${'Older'}</a></li>`
-              : null}
-            ${Boolean(state.next)
-              ? liAnchor`<li><a ${{
-                class: 'next',
-                rel: 'next',
-                ...anchorAttrs(link('/posts/:slug/', state.next))
-              }}>${'Newer'}</a></li>`
-              : null}
-          </ul>
-        </nav>
-      </main>`)
+          <div>${safe(state.post.html)}</div>
+          <nav class="pagination">
+            <ul>
+              ${Boolean(state.prev)
+                ? liAnchor`<li><a ${{
+                  class: 'prev',
+                  ...anchorAttrs(link('/posts/:slug/', state.prev))
+                }}>${'Older'}</a></li>`
+                : null}
+              ${Boolean(state.next)
+                ? liAnchor`<li><a ${{
+                  class: 'next',
+                  ...anchorAttrs(link('/posts/:slug/', state.next))
+                }}>${'Newer'}</a></li>`
+                : null}
+            </ul>
+          </nav>
+        </article>`)
 
-      on(() => error`<main>
-        <h1>${state.title}</h1>
-        <p>${state.error != null ? state.error.message : ''}</p>
-      </main>`)
-    })}
+        on(() => error`<section>
+          <h1>${state.title}</h1>
+          <p>${state.error != null ? state.error.message : ''}</p>
+        </section>`)
+      })}
+    </main>
     <footer class="footer">
       <ul>
         <li><a href="https://github.com/erickmerchant/my-blog">View Source</a></li>
