164e0750bc972471b96fbea324da74ea3e26ebb2
Erick Merchant <erickmerchant@gmail.com>
2019-09-05 00:41:07 -0400

    Try a service worker

--- postbuild.js
+++ postbuild.js
@@ -3,6 +3,7 @@ const path = require('path')
 const promisify = require('util').promisify
 const writeFile = promisify(fs.writeFile)
 const globby = require('globby')
+const version = require('./package.json').version
 
 ;(async () => {
   const posts = require('./dist/content/posts/index.json')
@@ -10,17 +11,20 @@ const globby = require('globby')
   const headers = [
     '  Link: </content/posts/index.json>; rel=preload; as=fetch; crossorigin=anonymous'
   ]
+  const relatives = []
 
   for (const file of files) {
-    const relative = path.relative('./dist', file)
+    const relative = `/${path.relative('./dist', file)}`
+
+    relatives.push(relative)
 
     switch (path.extname(relative)) {
       case '.css':
-        headers.push(`  Link: </${relative}>; rel=preload; as=style`)
+        headers.push(`  Link: <${relative}>; rel=preload; as=style`)
         break
 
       case '.mjs':
-        headers.push(`  Link: </${relative}>; rel=preload; as=script; crossorigin=anonymous`)
+        headers.push(`  Link: <${relative}>; rel=preload; as=script; crossorigin=anonymous`)
         break
     }
   }
@@ -40,4 +44,35 @@ const globby = require('globby')
   }
 
   await writeFile('./dist/_headers', lines.join('\n'))
+
+  await writeFile('./dist/sw.js', `
+    const version = ${JSON.stringify(version)} || '1'
+    const assets = ${JSON.stringify(relatives)}
+
+    self.addEventListener('install', (event) => {
+      event.waitUntil(
+        caches.open(version).then((cache) => cache.addAll(assets))
+      )
+    })
+
+    self.addEventListener('activate', (event) => {
+      event.waitUntil(
+        caches.keys().then((keys) => Promise.all(
+          keys.map((key) => {
+            if (key !== version) {
+              return caches.delete(key)
+            }
+          })
+        ))
+      )
+    })
+
+    self.addEventListener('fetch', (event) => {
+      const url = new URL(event.request.url)
+
+      if (assets.includes(url.pathname)) {
+        event.respondWith(caches.match(url.pathname))
+      }
+    })
+  `)
 })()
