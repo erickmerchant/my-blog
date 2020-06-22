9b55fa41aff64cce2573202844bb43eb06125484
Erick Merchant <erickmerchant@gmail.com>
2020-06-21 21:20:51 -0400

    Simplify cli.js and update

--- cli.js
+++ cli.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import sergeant from 'sergeant'
 import del from 'del'
 import {promisify} from 'util'
 import fs from 'fs'
@@ -7,86 +6,92 @@ import cheerio from 'cheerio'
 import execa from 'execa'
 import {stringify} from '@erickmerchant/framework/stringify.js'
 
-const {command, start} = sergeant('cli.js')
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
-const execaOptions = {shell: true, stdio: 'inherit', cwd: process.cwd()}
+const options = {stdio: 'inherit', preferLocal: true}
+const command = process.argv[2]
 
-command({
-  name: 'start',
-  async action() {
-    execa('css src/styles.js src/css/styles -wd', execaOptions)
+const program = async () => {
+  try {
+    if (command === 'start') {
+      execa('css', ['src/styles.js', 'src/css/styles', '-wd'], options)
 
-    execa('css src/editor/styles.js src/editor/css/styles -wd', execaOptions)
+      execa(
+        'css',
+        ['src/editor/styles.js', 'src/editor/css/styles', '-wd'],
+        options
+      )
 
-    execa('dev serve src -d -e dev.html', execaOptions)
-  }
-})
-
-command({
-  name: 'build',
-  async action() {
-    await Promise.all([
-      execa('css src/styles.js src/css/styles', execaOptions),
-      execa('dev cache src dist', execaOptions)
-    ])
-
-    await del(['./dist/editor/', './dist/editor.html'])
-
-    const paths = {
-      index: './dist/index.html',
-      styles: './dist/css/styles.css'
+      execa('dev', ['serve', 'src', '-d', '-e', 'dev.html'], options)
     }
 
-    const [
-      {classes},
-      {createComponent},
-      {getSegments, contentComponent}
-    ] = await Promise.all([
-      import('./src/css/styles.js'),
-      import('./src/main.js'),
-      import('./src/common.js')
-    ])
+    if (command === 'build') {
+      await Promise.all([
+        execa('css', ['src/styles.js', 'src/css/styles'], options),
+        execa('dev', ['cache', 'src', 'dist'], options)
+      ])
 
-    const state = {route: '', title: ''}
-    const component = createComponent(
-      {},
-      classes,
-      () => {},
-      getSegments,
-      contentComponent
-    )
+      await del(['./dist/editor/', './dist/editor.html'])
 
-    const $body = cheerio.load(stringify(component(state)))('body')
-
-    $body.find('*').each((index, el) => {
-      let node = el.firstChild
-
-      while (node) {
-        if (node.nodeType === 3) {
-          node.nodeValue = node.nodeValue.replace(/\s+/g, ' ')
-        }
-
-        node = node.nextSibling
+      const paths = {
+        index: './dist/index.html',
+        styles: './dist/css/styles.css'
       }
-    })
 
-    const [index, styles] = await Promise.all(
-      [paths.index, paths.styles].map((path) => readFile(path, 'utf8'))
-    )
+      const [
+        {classes},
+        {createComponent},
+        {getSegments, contentComponent}
+      ] = await Promise.all([
+        import('./src/css/styles.js'),
+        import('./src/main.js'),
+        import('./src/common.js')
+      ])
 
-    const styleHTML = `<style>${styles}</style>`
+      const state = {route: '', title: ''}
+      const component = createComponent(
+        {},
+        classes,
+        () => {},
+        getSegments,
+        contentComponent
+      )
 
-    const $ = cheerio.load(index)
+      const $body = cheerio.load(stringify(component(state)))('body')
 
-    $('link[rel="stylesheet"]').replaceWith(styleHTML)
+      $body.find('*').each((index, el) => {
+        let node = el.firstChild
 
-    $('body').attr('class', $body.attr('class')).html($body.html())
+        while (node) {
+          if (node.nodeType === 3) {
+            node.nodeValue = node.nodeValue.replace(/\s+/g, ' ')
+          }
 
-    await writeFile(paths.index, $.html())
+          node = node.nextSibling
+        }
+      })
 
-    await execa('rollup -c rollup.config.js', execaOptions)
+      const [index, styles] = await Promise.all(
+        [paths.index, paths.styles].map((path) => readFile(path, 'utf8'))
+      )
+
+      const styleHTML = `<style>${styles}</style>`
+
+      const $ = cheerio.load(index)
+
+      $('link[rel="stylesheet"]').replaceWith(styleHTML)
+
+      $('body').attr('class', $body.attr('class')).html($body.html())
+
+      await writeFile(paths.index, $.html())
+
+      await execa('rollup', ['-c', 'rollup.config.js'], options)
+    }
+  } catch (error) {
+    console.error(error)
+
+    process.exit(1)
   }
-})
+}
 
-start(process.argv.slice(2))
+program()
