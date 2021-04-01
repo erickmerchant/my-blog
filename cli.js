1e59ea8c2e66c19de271838aab816af8efc166f0
Erick Merchant <erickmerchant@gmail.com>
2021-03-31 23:59:55 -0400

    Use top-level await

--- cli.js
+++ cli.js
@@ -14,67 +14,63 @@ const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 const command = process.argv[2]
 
-const program = async () => {
-  try {
-    if (command === 'start') {
-      spawn`css src/styles.js dist/css -dw src`
+try {
+  if (command === 'start') {
+    spawn`css src/styles.js dist/css -dw src`
 
-      spawn`css src/editor/styles.js dist/editor/css -dw src/editor`
+    spawn`css src/editor/styles.js dist/editor/css -dw src/editor`
 
-      spawn`dev serve src dist -de dev.html`
-    }
-
-    if (command === 'build') {
-      await Promise.all([
-        spawn`css src/styles.js dist/css`,
-        spawn`dev cache src dist -i src/editor.html -i src/dev.html`
-      ])
-
-      const {layoutClasses, aboutClasses} = await import('./dist/css/styles.js')
-
-      const state = {title: ''}
-
-      const aboutView = createAboutView({
-        classes: aboutClasses,
-        contentView: createContentView({
-          templates: Object.assign(
-            getDefaultContentTemplates({classes: aboutClasses}),
-            getAboutContentTemplates({classes: aboutClasses})
-          )
-        })
-      })
-
-      const layoutView = createLayoutView({
-        classes: layoutClasses,
-        aboutView,
-        mainView: () =>
-          html`
-            <article id="main"></article>
-          `,
-        anchorAttrs: (href) => {
-          return {href}
-        }
-      })
-
-      const indexView = createIndexView({layoutView})
-
-      await Promise.all([
-        spawn`rollup -c rollup.config.js`,
-        spawn`postcss ./dist/css/styles.css --no-map -u cssnano -o ./dist/css/styles.css`
-      ])
-
-      state.styles = await readFile('./dist/css/styles.css', 'utf8')
-
-      await writeFile(
-        './dist/index.html',
-        `<!doctype html>${stringify(indexView(state))}`
-      )
-    }
-  } catch (error) {
-    console.error(error)
-
-    process.exit(1)
+    spawn`dev serve src dist -de dev.html`
   }
-}
 
-program()
+  if (command === 'build') {
+    await Promise.all([
+      spawn`css src/styles.js dist/css`,
+      spawn`dev cache src dist -i src/editor.html -i src/dev.html`
+    ])
+
+    const {layoutClasses, aboutClasses} = await import('./dist/css/styles.js')
+
+    const state = {title: ''}
+
+    const aboutView = createAboutView({
+      classes: aboutClasses,
+      contentView: createContentView({
+        templates: Object.assign(
+          getDefaultContentTemplates({classes: aboutClasses}),
+          getAboutContentTemplates({classes: aboutClasses})
+        )
+      })
+    })
+
+    const layoutView = createLayoutView({
+      classes: layoutClasses,
+      aboutView,
+      mainView: () =>
+        html`
+          <article id="main"></article>
+        `,
+      anchorAttrs: (href) => {
+        return {href}
+      }
+    })
+
+    const indexView = createIndexView({layoutView})
+
+    await Promise.all([
+      spawn`rollup -c rollup.config.js`,
+      spawn`postcss ./dist/css/styles.css --no-map -u cssnano -o ./dist/css/styles.css`
+    ])
+
+    state.styles = await readFile('./dist/css/styles.css', 'utf8')
+
+    await writeFile(
+      './dist/index.html',
+      `<!doctype html>${stringify(indexView(state))}`
+    )
+  }
+} catch (error) {
+  console.error(error)
+
+  process.exit(1)
+}
