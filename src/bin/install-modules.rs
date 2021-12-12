f06f4b30d9c127c56878a60c6ce419b5388c1277
erickmerchant <erickmerchant@gmail.com>
2021-12-12 13:06:40 -0500

    Improve install-modules

--- src/bin/install-modules.rs
+++ src/bin/install-modules.rs
@@ -3,17 +3,16 @@ use std::path::Path;
 use url::Url;
 
 fn main() {
-  let deps = [(
-    "https://cdn.skypack.dev/@hyper-views/framework?min",
-    "@hyper-views/framework/main.js",
-  )];
+  let deps = ["@hyper-views/framework"];
+  let domain = "https://cdn.skypack.dev";
 
-  for (url, destination) in &deps {
-    let res = reqwest::blocking::get(url.to_string()).expect("Failed to get from cdn.skypack.dev");
+  for dep in &deps {
+    let res = reqwest::blocking::get(format!("{}/{}?min", domain, dep).to_string())
+      .expect("Failed to get from cdn.skypack.dev");
     let headers = res.headers();
 
     let res = reqwest::blocking::get(
-      Url::parse("https://cdn.skypack.dev")
+      Url::parse(domain)
         .unwrap()
         .join(
           headers["x-import-url"]
@@ -26,7 +25,7 @@ fn main() {
     .unwrap();
 
     let body = res.text().expect("Empty body");
-    let full_destination = Path::new("modules").join(destination);
+    let full_destination = Path::new("modules").join(format!("{}/index.js", dep));
 
     fs::create_dir_all(full_destination.parent().unwrap()).unwrap();
 
