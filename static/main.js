79f4706b4c9886e209dae32e5b1c81b4ac7cf6d3
erickmerchant <erickmerchant@gmail.com>
2022-01-21 23:01:28 -0500

    Start work on nav

--- static/main.js
+++ static/main.js
@@ -4,9 +4,24 @@ register(
   class {
     static tag = "site-nav";
 
+    open = false;
+
+    toggleOpen = () => {
+      this.open = !this.open;
+
+      document.body.classList.toggle("nav--open", this.open);
+
+      render(this);
+    };
+
     template = () => html`
       <div>
-        <slot />
+        <button part="toggle" type="button" @click=${this.toggleOpen}>
+          ${this.open ? "Close" : "Menu"}
+        </button>
+        <nav part="nav">
+          <slot />
+        </nav>
       </div>
     `;
   }
