b94dd2d5cbddc63fa1cc305a95660dc99c833d7f
erickmerchant <erickmerchant@gmail.com>
2022-01-24 23:53:37 -0500

    More work on nav

--- static/main.js
+++ static/main.js
@@ -1,5 +1,36 @@
 import { render, register, html } from "/vendor/@hyper-views/framework/main.js";
 
+const closeIcon = html`
+  <svg
+    viewBox="0 0 100 100"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    class="Icon self"
+  >
+    <rect
+      height="20"
+      width="120"
+      transform="rotate(-45,50,50)"
+      x="-10"
+      y="40"
+    />
+    <rect height="20" width="120" transform="rotate(45,50,50)" x="-10" y="40" />
+  </svg>
+`;
+
+const menuIcon = html`
+  <svg
+    viewBox="0 0 100 100"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    class="Icon self"
+  >
+    <rect x="0" y="0" height="20" width="100" />
+    <rect x="0" y="40" height="20" width="100" />
+    <rect x="0" y="80" height="20" width="100" />
+  </svg>
+`;
+
 register(
   class {
     static tag = "page-layout";
@@ -21,25 +52,18 @@ register(
             display: none;
           }
         </style>
-        <div part="toggle">
+        <nav>
           <button
-            class="Button root"
+            part="toggle"
+            class="Button self"
             type="button"
-            aria-label=${this.isOpen ? "Close" : "Menu"}
+            aria-label=${this.isOpen ? "Close nav" : "Open nav"}
+            aria-expanded=${this.isOpen ? "true" : "false"}
             @click=${this.toggleOpen}
           >
-            <div
-              class=${`Icon ${!this.isOpen ? "open" : "close"}`}
-              aria-hidden="true"
-            >
-              ${(!this.isOpen ? ["", "", ""] : ["", ""]).map(
-                () => html`<span />`
-              )}
-            </div>
+            ${this.isOpen ? closeIcon : menuIcon}
           </button>
-        </div>
-        <nav part="nav">
-          <slot name="nav" />
+          <div part="nav"><slot name="nav" /></div>
         </nav>
         <div part="panel">
           <slot name="panel" />
