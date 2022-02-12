b6235656974703dce138e8c60061b8bc9018e30d
erickmerchant <erickmerchant@gmail.com>
2022-02-11 21:46:17 -0500

    Prepare for hosting

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,40 @@ use actix_web::{
     middleware::{Compress, ErrorHandlers, Logger},
     web, App, HttpServer,
 };
-use dotenv::dotenv;
-use handlebars::Handlebars;
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
-use std::{env, fs, io};
 
+use handlebars::Handlebars;
+use std::io;
+
+pub fn configure_app(cfg: &mut web::ServiceConfig) {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".hbs", "./templates")
+        .expect("Templates");
+    let handlebars_ref = web::Data::new(handlebars);
+
+    cfg.service(
+        web::scope("")
+            .wrap(Logger::new("%s %r"))
+            .wrap(ErrorHandlers::new().handler(StatusCode::NOT_FOUND, pages::not_found))
+            .wrap(
+                ErrorHandlers::new()
+                    .handler(StatusCode::INTERNAL_SERVER_ERROR, pages::internal_error),
+            )
+            .wrap(Compress::default())
+            .app_data(handlebars_ref.clone())
+            .route("/", web::get().to(pages::index))
+            .route("/{file:.*.html}", web::get().to(pages::page))
+            .route("/{file:.*?}", web::get().to(assets::file)),
+    );
+}
+
+#[cfg(feature = "local-dev")]
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    use dotenv::dotenv;
+    use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+    use std::{env, fs};
+
     dotenv().expect("Failed to read .env file");
 
     env_logger::init();
@@ -30,30 +57,20 @@ async fn main() -> io::Result<()> {
         env::var("SSL_CERT").expect("Failed to read env variable SSL_CERT"),
     )?;
 
-    HttpServer::new(move || {
-        let mut handlebars = Handlebars::new();
-        handlebars
-            .register_templates_directory(".hbs", "./templates")
-            .expect("Templates");
-        let handlebars_ref = web::Data::new(handlebars);
+    HttpServer::new(move || App::new().configure(configure_app))
+        .bind_openssl(
+            env::var("BIND_ADDRESS").expect("Failed to read env variable BIND_ADDRESS"),
+            ssl_builder,
+        )?
+        .run()
+        .await
+}
 
-        App::new()
-            .wrap(Logger::new("%s %r"))
-            .wrap(ErrorHandlers::new().handler(StatusCode::NOT_FOUND, pages::not_found))
-            .wrap(
-                ErrorHandlers::new()
-                    .handler(StatusCode::INTERNAL_SERVER_ERROR, pages::internal_error),
-            )
-            .wrap(Compress::default())
-            .app_data(handlebars_ref.clone())
-            .route("/", web::get().to(pages::index))
-            .route("/{file:.*.html}", web::get().to(pages::page))
-            .route("/{file:.*?}", web::get().to(assets::file))
-    })
-    .bind_openssl(
-        env::var("BIND_ADDRESS").expect("Failed to read env variable BIND_ADDRESS"),
-        ssl_builder,
-    )?
-    .run()
-    .await
+#[cfg(not(feature = "local-dev"))]
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(move || App::new().configure(configure_app))
+        .bind("0.0.0.0:1000")?
+        .run()
+        .await
 }
