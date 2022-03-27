76d678d8c83de663a50ff76849951c05f8f97798
erickmerchant <erickmerchant@gmail.com>
2022-03-27 01:04:14 -0400

    Rewrite install-vendors script

--- src/bin/install-vendors.rs
+++ src/bin/install-vendors.rs
@@ -1,3 +1,5 @@
+use actix_rt::System;
+use awc::Client;
 use std::{convert::AsRef, fs, path::Path};
 
 struct Dependency<S: AsRef<str>> {
@@ -7,26 +9,29 @@ struct Dependency<S: AsRef<str>> {
 }
 
 fn main() {
-  let domain = "https://unpkg.com";
-  let deps = [Dependency {
-    name: "@hyper-views/dom",
-    version: "1",
-    file: "main.js",
-  }];
+  System::new().block_on(async {
+    let client = Client::default();
+    let domain = "https://unpkg.com";
+    let deps = [Dependency {
+      name: "@hyper-views/dom",
+      version: "1",
+      file: "main.js",
+    }];
 
-  fs::remove_dir_all("assets/vendor").ok();
+    fs::remove_dir_all("assets/vendor").ok();
 
-  for dep in deps {
-    let res = reqwest::blocking::get(
-      format!("{}/{}@{}/{}", domain, dep.name, dep.version, dep.file).to_string(),
-    )
-    .expect("Failed to get from cdn.skypack.dev");
+    for dep in deps {
+      let mut res = client
+        .get(format!("{}/{}@{}/{}", domain, dep.name, dep.version, dep.file).to_string())
+        .send()
+        .await
+        .unwrap();
 
-    let body = res.text().expect("Empty body");
-    let full_destination = Path::new("assets/vendor").join(dep.name).join(dep.file);
+      let full_destination = Path::new("assets/vendor").join(dep.name).join(dep.file);
 
-    fs::create_dir_all(full_destination.parent().unwrap()).unwrap();
+      fs::create_dir_all(full_destination.parent().unwrap()).unwrap();
 
-    fs::write(full_destination, body).unwrap();
-  }
+      fs::write(full_destination, res.body().await.unwrap()).unwrap();
+    }
+  });
 }
