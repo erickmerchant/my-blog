f570af876718ccc640b529040312fd49be726725
erickmerchant <erickmerchant@gmail.com>
2022-04-23 23:52:52 -0400

    Remove ssl for dev

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,13 @@ use actix_web::{
         StatusCode,
     },
     middleware::{Compress, ErrorHandlerResponse, ErrorHandlers, Logger},
-    App, HttpServer, Result,
+    web, App, HttpServer, Result,
 };
-use std::{env, io};
+use std::io;
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     use dotenv::dotenv;
-    use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
     use std::fs;
 
     dotenv().expect("failed to read .env file");
@@ -25,29 +24,22 @@ async fn main() -> io::Result<()> {
 
     fs::remove_dir_all("storage/cache").ok();
 
-    let mut ssl_builder = SslAcceptor::mozilla_intermediate(SslMethod::tls())?;
-
-    ssl_builder.set_private_key_file(
-        env::var("SSL_KEY").expect("failed to read env variable SSL_KEY"),
-        SslFiletype::PEM,
-    )?;
-    ssl_builder.set_certificate_chain_file(
-        env::var("SSL_CERT").expect("failed to read env variable SSL_CERT"),
-    )?;
-
-    let port = env::var("PORT").expect("failed to read env variable PORT");
-
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::new("%s %r"))
             .wrap(ErrorHandlers::new().handler(StatusCode::NOT_FOUND, not_found))
             .wrap(ErrorHandlers::new().handler(StatusCode::INTERNAL_SERVER_ERROR, internal_error))
             .wrap(Compress::default())
+            .route("/health", web::get().to(health))
             .configure(service::configure)
     })
-    .bind_openssl(format!("0.0.0.0:{port}"), ssl_builder)?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
+}
+
+async fn health() -> Result<String> {
+    Ok("OK".to_string())
 }
 
 fn not_found<B>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
