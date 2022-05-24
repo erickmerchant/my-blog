424f1dce4d585c4cdd01a9ed70e8206375c84d6a
erickmerchant <erickmerchant@gmail.com>
2022-05-24 00:54:53 -0400

    Swipe and sticky nav

--- assets/site.js
+++ assets/site.js
@@ -25,16 +25,18 @@ class PageApp extends HTMLElement {
     this.update();
   };
 
+  touchState = null;
+
   update = () => {
     this.setAttribute("color-scheme", this.colorScheme);
 
     this.toggleAttribute("open", this.open);
 
-    for (const anchor of this.querySelectorAll('[slot="links"] a')) {
+    for (let anchor of this.querySelectorAll('[slot="links"] a')) {
       anchor.setAttribute("tabIndex", !this.open ? "-1" : "0");
     }
 
-    for (const anchor of this.querySelectorAll('[slot="panel"] a')) {
+    for (let anchor of this.querySelectorAll('[slot="panel"] a')) {
       anchor.setAttribute("tabIndex", this.open ? "-1" : "0");
     }
 
@@ -47,39 +49,38 @@ class PageApp extends HTMLElement {
     </style>
 
     <nav class="nav">
-      <div class="nav-toggle">
-        <button
-          type="button"
-          aria-expanded=${this.open ? "true" : "false"}
-          aria-label="Toggle nav"
-          @click=${this.toggleOpen}
-        >
-          <svg class="icon" viewBox="0 0 100 100" aria-hidden="true">
-            ${this.open
-              ? html`
-                  <rect
-                    height="20"
-                    width="120"
-                    transform="rotate(-45,50,50)"
-                    x="-10"
-                    y="40"
-                  />
-                  <rect
-                    height="20"
-                    width="120"
-                    transform="rotate(45,50,50)"
-                    x="-10"
-                    y="40"
-                  />
-                `
-              : html`
-                  <rect x="0" y="0" transform="" height="20" width="100" />
-                  <rect x="0" y="40" transform="" height="20" width="100" />
-                  <rect x="0" y="80" transform="" height="20" width="100" />
-                `}
-          </svg>
-        </button>
-      </div>
+      <button
+        class="nav-toggle"
+        type="button"
+        aria-expanded=${this.open ? "true" : "false"}
+        aria-label="Toggle nav"
+        @click=${this.toggleOpen}
+      >
+        <svg class="icon" viewBox="0 0 100 100" aria-hidden="true">
+          ${this.open
+            ? html`
+                <rect
+                  height="20"
+                  width="120"
+                  transform="rotate(-45,50,50)"
+                  x="-10"
+                  y="40"
+                />
+                <rect
+                  height="20"
+                  width="120"
+                  transform="rotate(45,50,50)"
+                  x="-10"
+                  y="40"
+                />
+              `
+            : html`
+                <rect x="0" y="0" transform="" height="20" width="100" />
+                <rect x="0" y="40" transform="" height="20" width="100" />
+                <rect x="0" y="80" transform="" height="20" width="100" />
+              `}
+        </svg>
+      </button>
 
       <div
         class="nav-content"
@@ -142,6 +143,56 @@ class PageApp extends HTMLElement {
 
       this.update();
     });
+
+    this.addEventListener(
+      "touchstart",
+      function (e) {
+        let firstTouch = e.touches?.[0];
+
+        this.touchState = firstTouch
+          ? {
+              clientX: firstTouch.clientX,
+              clientY: firstTouch.clientY,
+              isSwipe: false,
+            }
+          : null;
+      },
+      { passive: true }
+    );
+
+    this.addEventListener(
+      "touchmove",
+      function (e) {
+        let touchMove = e.touches?.[0];
+
+        let touchState = this.touchState;
+
+        if (touchMove && touchState) {
+          if (
+            Math.abs(touchState.clientX - touchMove.clientX) >
+            Math.abs(touchState.clientY - touchMove.clientY)
+          ) {
+            if (
+              (touchState.clientX > touchMove.clientX && this.open) ||
+              (touchMove.clientX > touchState.clientX && !this.open)
+            ) {
+              this.touchState.isSwipe = true;
+            }
+          }
+        }
+      },
+      { passive: true }
+    );
+
+    this.addEventListener(
+      "touchend",
+      function (e) {
+        if (this.touchState?.isSwipe) {
+          this.toggleOpen();
+        }
+      },
+      { passive: true }
+    );
   }
 }
 
