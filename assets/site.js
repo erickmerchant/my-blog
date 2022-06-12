6cdefdb45813421bd48347bcc16058206749aecc
erickmerchant <erickmerchant@gmail.com>
2022-06-12 12:59:23 -0400

    Move custom element defines

--- assets/site.js
+++ assets/site.js
@@ -1,185 +1,191 @@
-class PageApp extends HTMLElement {
-  open = false;
+customElements.define(
+  "page-app",
+  class extends HTMLElement {
+    open = false;
 
-  toggleOpen = () => {
-    this.open = !this.open;
+    toggleOpen = () => {
+      this.open = !this.open;
 
-    this.updateLayout();
-  };
+      this.updateLayout();
+    };
 
-  updateLayout = () => {
-    this.toggleAttribute("open", this.open);
+    updateLayout = () => {
+      this.toggleAttribute("open", this.open);
 
-    const shadow = this.shadowRoot;
+      const shadow = this.shadowRoot;
 
-    for (let input of shadow.querySelectorAll("[name='color-scheme-option']")) {
-      input.setAttribute("tabindex", !this.open ? "-1" : "0");
-    }
-
-    shadow
-      .querySelector("[name='toggle']")
-      .setAttribute("aria-expanded", this.open ? "true" : "false");
-
-    shadow
-      .querySelector("[name='toggle'] slot-match")
-      .setAttribute("name", this.open ? "close" : "open");
-
-    const tabs = [
-      ["nav", this.open],
-      ["panel", !this.open],
-    ];
-
-    for (const [tab, active] of tabs) {
-      const el = shadow.getElementById(tab);
-
-      if (active) {
-        el.removeAttribute("aria-hidden");
-      } else {
-        el.setAttribute("aria-hidden", "true");
+      for (let input of shadow.querySelectorAll(
+        "[name='color-scheme-option']"
+      )) {
+        input.setAttribute("tabindex", !this.open ? "-1" : "0");
       }
 
-      for (let anchor of this.querySelectorAll(`[slot="${tab}"] a`)) {
-        anchor.setAttribute("tabindex", active ? "0" : "-1");
+      shadow
+        .querySelector("[name='toggle']")
+        .setAttribute("aria-expanded", this.open ? "true" : "false");
+
+      shadow
+        .querySelector("[name='toggle'] slot-match")
+        .setAttribute("name", this.open ? "close" : "open");
+
+      const tabs = [
+        ["nav", this.open],
+        ["panel", !this.open],
+      ];
+
+      for (const [tab, active] of tabs) {
+        const el = shadow.getElementById(tab);
+
+        if (active) {
+          el.removeAttribute("aria-hidden");
+        } else {
+          el.setAttribute("aria-hidden", "true");
+        }
+
+        for (let anchor of this.querySelectorAll(`[slot="${tab}"] a`)) {
+          anchor.setAttribute("tabindex", active ? "0" : "-1");
+        }
       }
-    }
-  };
+    };
 
-  prefersColorSchemeDark = window.matchMedia("(prefers-color-scheme: dark)");
+    prefersColorSchemeDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-  initColorScheme = () =>
-    window.localStorage.getItem("color-scheme") ??
-    (this.prefersColorSchemeDark.matches ? "dark" : "light");
+    initColorScheme = () =>
+      window.localStorage.getItem("color-scheme") ??
+      (this.prefersColorSchemeDark.matches ? "dark" : "light");
 
-  colorScheme = this.initColorScheme();
+    colorScheme = this.initColorScheme();
 
-  changeColorScheme = (value) => {
-    this.colorScheme = value;
+    changeColorScheme = (value) => {
+      this.colorScheme = value;
 
-    window.localStorage.setItem("color-scheme", this.colorScheme);
+      window.localStorage.setItem("color-scheme", this.colorScheme);
 
-    this.updateColorScheme();
-  };
+      this.updateColorScheme();
+    };
 
-  updateColorScheme = () => {
-    const shadow = this.shadowRoot;
+    updateColorScheme = () => {
+      const shadow = this.shadowRoot;
 
-    this.setAttribute("color-scheme", this.colorScheme);
+      this.setAttribute("color-scheme", this.colorScheme);
 
-    for (let input of shadow.querySelectorAll("[name='color-scheme-option']")) {
-      input.checked = this.colorScheme === input.value;
-    }
-  };
+      for (let input of shadow.querySelectorAll(
+        "[name='color-scheme-option']"
+      )) {
+        input.checked = this.colorScheme === input.value;
+      }
+    };
 
-  connectedCallback() {
-    this.attachShadow({ mode: "open" });
+    connectedCallback() {
+      this.attachShadow({ mode: "open" });
 
-    const shadow = this.shadowRoot;
+      const shadow = this.shadowRoot;
 
-    shadow.append(this.querySelector("template").content);
+      shadow.append(this.querySelector("template").content);
 
-    shadow
-      .querySelector("[name='toggle']")
-      .addEventListener("click", this.toggleOpen);
+      shadow
+        .querySelector("[name='toggle']")
+        .addEventListener("click", this.toggleOpen);
 
-    shadow
-      .querySelector("[name='color-scheme']")
-      .addEventListener("change", (e) => {
-        if (e.target.matches("[name='color-scheme-option']")) {
-          this.changeColorScheme(e.target.value);
+      shadow
+        .querySelector("[name='color-scheme']")
+        .addEventListener("change", (e) => {
+          if (e.target.matches("[name='color-scheme-option']")) {
+            this.changeColorScheme(e.target.value);
+          }
+        });
+
+      this.addEventListener("click", (e) => {
+        if (e.target.matches("a")) {
+          const href = e.target.href;
+
+          if (new URL(href).hostname === window.location.hostname) {
+            if (this.open) {
+              e.preventDefault();
+
+              shadow.addEventListener(
+                "transitionend",
+                () => {
+                  window.location.assign(href);
+                },
+                { once: true }
+              );
+
+              this.toggleOpen();
+            }
+          }
         }
       });
 
-    this.addEventListener("click", (e) => {
-      if (e.target.matches("a")) {
-        const href = e.target.href;
+      shadow.getElementById("panel").addEventListener("click", () => {
+        if (this.open) this.toggleOpen();
+      });
 
-        if (new URL(href).hostname === window.location.hostname) {
-          if (this.open) {
-            e.preventDefault();
+      this.prefersColorSchemeDark.addEventListener("change", () => {
+        this.colorScheme = this.initColorScheme();
 
-            shadow.addEventListener(
-              "transitionend",
-              () => {
-                window.location.assign(href);
-              },
-              { once: true }
-            );
+        this.updateColorScheme();
+      });
 
-            this.toggleOpen();
-          }
-        }
-      }
-    });
-
-    shadow.getElementById("panel").addEventListener("click", () => {
-      if (this.open) this.toggleOpen();
-    });
-
-    this.prefersColorSchemeDark.addEventListener("change", () => {
-      this.colorScheme = this.initColorScheme();
+      this.updateLayout();
 
       this.updateColorScheme();
-    });
-
-    this.updateLayout();
-
-    this.updateColorScheme();
-  }
-}
-
-class CodeBlock extends HTMLElement {
-  connectedCallback() {
-    this.attachShadow({ mode: "open" });
-
-    const shadow = this.shadowRoot;
-
-    shadow.innerHTML =
-      '<style>@import "/site.css";</style><pre class="code-block"><code></code></pre>';
-
-    const code = shadow.querySelector("code");
-
-    for (const ln of this.innerHTML.trim().split("\n")) {
-      const marker = document.createElement("span");
-
-      marker.setAttribute("class", "number");
-
-      code.append(marker);
-
-      const line = document.createElement("span");
-
-      line.setAttribute("class", "line");
-
-      line.innerHTML = ln ? ln : " ";
-
-      code.append(line);
     }
   }
-}
+);
 
-class SlotMatch extends HTMLElement {
-  static get observedAttributes() {
-    return ["name"];
+customElements.define(
+  "code-block",
+  class extends HTMLElement {
+    connectedCallback() {
+      this.attachShadow({ mode: "open" });
+
+      const shadow = this.shadowRoot;
+
+      shadow.innerHTML = `<style>@import "/site.css";</style><pre class="code-block"><code></code></pre>`;
+
+      const code = shadow.querySelector("code");
+
+      for (const ln of this.innerHTML.trim().split("\n")) {
+        const marker = document.createElement("span");
+
+        marker.setAttribute("class", "number");
+
+        code.append(marker);
+
+        const line = document.createElement("span");
+
+        line.setAttribute("class", "line");
+
+        line.innerHTML = ln ? ln : " ";
+
+        code.append(line);
+      }
+    }
   }
+);
 
-  attributeChangedCallback(name, _, newValue) {
-    this.shadowRoot?.querySelector("slot")?.setAttribute(name, newValue);
+customElements.define(
+  "slot-match",
+  class extends HTMLElement {
+    static get observedAttributes() {
+      return ["name"];
+    }
+
+    attributeChangedCallback(name, _, newValue) {
+      this.shadowRoot?.querySelector("slot")?.setAttribute(name, newValue);
+    }
+
+    connectedCallback() {
+      this.attachShadow({ mode: "open" });
+
+      const shadow = this.shadowRoot;
+
+      shadow.innerHTML = `<style>@import "/site.css";</style><slot></slot>`;
+
+      const slot = shadow.querySelector("slot");
+
+      slot.setAttribute("name", this.getAttribute("name"));
+    }
   }
-
-  connectedCallback() {
-    this.attachShadow({ mode: "open" });
-
-    const shadow = this.shadowRoot;
-
-    shadow.innerHTML = `<style>@import "/site.css";</style><slot></slot>`;
-
-    const slot = shadow.querySelector("slot");
-
-    slot.setAttribute("name", this.getAttribute("name"));
-  }
-}
-
-customElements.define("page-app", PageApp);
-
-customElements.define("code-block", CodeBlock);
-
-customElements.define("slot-match", SlotMatch);
+);
