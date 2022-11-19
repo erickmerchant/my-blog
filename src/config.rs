d52258b5a1c5541ed3d024f70321754c9ebaf55e
erickmerchant <erickmerchant@gmail.com>
2022-11-19 02:39:46 -0500

    Increase readability

--- src/config.rs
+++ src/config.rs
@@ -10,15 +10,19 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Self {
+        let source_maps = env::var("SOURCE_MAPS")
+            .map(|val| val == *"true")
+            .unwrap_or(false);
+        let port = env::var("PORT")
+            .map(|val| val.parse::<u16>().unwrap_or(8080))
+            .unwrap_or(8080);
+        let targets = env::var("TARGETS")
+            .unwrap_or_else(|_| String::from("supports es6-module and last 2 versions"));
+
         Self {
-            source_maps: env::var("SOURCE_MAPS")
-                .map(|val| val == *"true")
-                .unwrap_or(false),
-            port: env::var("PORT")
-                .map(|val| val.parse::<u16>().unwrap_or(8080))
-                .unwrap_or(8080),
-            targets: env::var("TARGETS")
-                .unwrap_or_else(|_| String::from("supports es6-module and last 2 versions")),
+            source_maps,
+            port,
+            targets,
         }
     }
 }
