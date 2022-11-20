1c5e1368c84bdb9ddaebea4690ee3691c8650b72
erickmerchant <erickmerchant@gmail.com>
2022-11-20 08:37:15 -0500

    Remove unused

--- src/assets/asset.rs
+++ src/assets/asset.rs
@@ -1,12 +1,9 @@
-use crate::{config, responses};
+use crate::responses;
 use actix_files::NamedFile;
 use actix_web::{error::ErrorNotFound, web, Result};
 use std::{fs::File, path::Path};
 
-pub async fn asset(
-    file: web::Path<String>,
-    _config: web::Data<config::Config>,
-) -> Result<NamedFile> {
+pub async fn asset(file: web::Path<String>) -> Result<NamedFile> {
     let src = Path::new("assets").join(file.to_string());
     let file = File::open(&src).map_err(ErrorNotFound)?;
 
