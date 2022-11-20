75cc31f9e671216edf6bb1eb32f685d155e24ca6
erickmerchant <erickmerchant@gmail.com>
2022-11-20 00:36:00 -0500

    Fix extensionless 404s

--- src/assets/asset.rs
+++ src/assets/asset.rs
@@ -1,6 +1,6 @@
 use crate::{config, responses};
 use actix_files::NamedFile;
-use actix_web::{error::ErrorInternalServerError, web, Result};
+use actix_web::{error::ErrorNotFound, web, Result};
 use std::{fs::File, path::Path};
 
 pub async fn asset(
@@ -8,7 +8,7 @@ pub async fn asset(
     _config: web::Data<config::Config>,
 ) -> Result<NamedFile> {
     let src = Path::new("assets").join(file.to_string());
-    let file = File::open(&src).map_err(ErrorInternalServerError)?;
+    let file = File::open(&src).map_err(ErrorNotFound)?;
 
     responses::file(file, src)
 }
