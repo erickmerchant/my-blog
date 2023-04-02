edb6a1a077049efb3400e63ec22cf70a7a34810f
erickmerchant <erickmerchant@gmail.com>
2023-04-01 22:16:07 -0400

    Move templates to theme

--- app/src/routes/asset.rs
+++ app/src/routes/asset.rs
@@ -3,12 +3,17 @@ use actix_web::{error::ErrorNotFound, web, Result};
 use std::{fs::File, path::Path};
 
 pub async fn asset(file: web::Path<String>) -> Result<NamedFile> {
-    let src = Path::new("theme").join(file.to_string());
+    match file.to_string().ends_with(".jinja") {
+        true => Err(ErrorNotFound("not found")),
+        false => {
+            let src = Path::new("theme").join(file.to_string());
 
-    let file = File::options()
-        .read(true)
-        .open(&src)
-        .map_err(ErrorNotFound)?;
+            let file = File::options()
+                .read(true)
+                .open(&src)
+                .map_err(ErrorNotFound)?;
 
-    super::file(file, src)
+            super::file(file, src)
+        }
+    }
 }
