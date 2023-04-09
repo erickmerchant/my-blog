64a2b0ada5c84c113026699bd1a50466ff4ba5e3
erickmerchant <erickmerchant@gmail.com>
2023-04-09 12:04:41 -0400

    Revert to static block

--- theme/elements/page-nav.js
+++ theme/elements/page-nav.js
@@ -1,32 +1,34 @@
 import {Element} from "../element.js";
 
-let previousY = 0;
-let frameRequested = false;
-
-document.body.addEventListener("scroll", () => {
-  if (!frameRequested) {
-    frameRequested = true;
-
-    window.requestAnimationFrame(() => {
-      frameRequested = false;
-
-      let currentY = document.body.scrollTop;
-
-      if (currentY !== previousY) {
-        document.body.style.setProperty(
-          "--scrolling-down",
-          currentY < previousY ? "0" : "1"
-        );
-      }
-
-      previousY = currentY;
-    });
-  }
-});
-
 customElements.define(
   "page-nav",
   class PageNav extends Element {
+    static {
+      let previousY = 0;
+      let frameRequested = false;
+
+      document.body.addEventListener("scroll", () => {
+        if (!frameRequested) {
+          frameRequested = true;
+
+          window.requestAnimationFrame(() => {
+            frameRequested = false;
+
+            let currentY = document.body.scrollTop;
+
+            if (currentY !== previousY) {
+              document.body.style.setProperty(
+                "--scrolling-down",
+                currentY < previousY ? "0" : "1"
+              );
+            }
+
+            previousY = currentY;
+          });
+        }
+      });
+    }
+
     static #icons = {
       open: "M1 1 l14 0 l0 3.5 l-14 0 z m0 5.25 l14 0 l0 3.5 l-14 0 z m0 5.25 l14 0 l0 3.5 l-14 0 z",
       close: "M1 4 l3 -3 l11 11 l-3 3 z m11 -3 l3 3 l-11 11 l-3 -3 z",
