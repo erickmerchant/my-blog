a00a0f85354100ad178109d12565223379091afc
erickmerchant <erickmerchant@gmail.com>
2023-04-22 00:45:01 -0400

    HTML

--- app/src/routes/index.rs
+++ app/src/routes/index.rs
@@ -1,5 +1,7 @@
 use crate::{models::page, routes::not_found, templates::minify_html, AppError, AppState};
-use axum::{extract::Path, extract::State, response::IntoResponse, response::Response};
+use axum::{
+    extract::Path, extract::State, response::Html, response::IntoResponse, response::Response,
+};
 use minijinja::context;
 use sea_orm::{entity::prelude::*, query::*};
 use std::{sync::Arc, vec::Vec};
@@ -41,7 +43,7 @@ pub async fn index(
 
             let html = minify_html(html);
 
-            Ok(html.into_response())
+            Ok(Html(html).into_response())
         }
         false => Ok(not_found(State(app_state))),
     }
--- app/src/routes/page.rs
+++ app/src/routes/page.rs
@@ -1,5 +1,7 @@
-use crate::{models::page, templates::minify_html, AppError, AppState};
-use axum::{extract::Path, extract::State, response::Response};
+use crate::{models::page, routes::not_found, templates::minify_html, AppError, AppState};
+use axum::{
+    extract::Path, extract::State, response::Html, response::IntoResponse, response::Response,
+};
 use minijinja::context;
 use sea_orm::{entity::prelude::*, query::*};
 use std::{path, sync::Arc};
@@ -60,9 +62,9 @@ pub async fn page(
                 .and_then(|template| template.render(ctx))?;
 
             let html = minify_html(html);
-        }
-        None => Err(ErrorNotFound("not found"))?,
-    };
 
-    // super::file(file, src)
+            Ok(Html(html).into_response())
+        }
+        None => Ok(not_found(State(app_state))),
+    }
 }
