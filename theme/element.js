c7b8691b3e54e6f807194b9eb348ec3a6bfa1085
erickmerchant <erickmerchant@gmail.com>
2023-08-24 21:56:45 -0400

    Change element.js api to allow usage outside custom elements

--- theme/element.js
+++ theme/element.js
@@ -1,76 +1,69 @@
+let current;
+let queue = [];
+let values = {};
+let reads = {};
+
+export let update = (updates) => {
+	let length = queue.length;
+
+	queue.push(...updates);
+
+	if (length === 0) {
+		setTimeout(() => {
+			let prev = current;
+
+			for (let u of queue.splice(0, Infinity)) {
+				current = u;
+				u();
+			}
+
+			current = prev;
+		}, 0);
+	}
+};
+
+export let watch = (object, defaults = object, set = () => {}) => {
+	for (let [k, initial] of Object.entries(defaults)) {
+		let symbol = Symbol(k);
+
+		reads[symbol] = [];
+
+		Object.defineProperty(object, k, {
+			get: () => {
+				if (current) {
+					reads[symbol].push(current);
+				}
+
+				return values[symbol];
+			},
+			set: (v) => {
+				if (values[symbol] !== v) {
+					values[symbol] = v;
+					set?.(k, v);
+					update([...new Set(reads[symbol].splice(0, Infinity))]);
+				}
+			},
+		});
+
+		object[k] = initial;
+	}
+
+	return object;
+};
+
 export class Element extends HTMLElement {
 	static get observedAttributes() {
 		return Object.keys(this.observedAttributeDefaults ?? {});
 	}
 
-	static #current;
-	static #queue = [];
-
-	static #update(updates) {
-		let length = this.#queue.length;
-
-		this.#queue.push(...updates);
-
-		if (length === 0) {
-			setTimeout(() => {
-				let prev = this.#current;
-
-				for (let u of this.#queue.splice(0, Infinity)) {
-					this.#current = u;
-					u();
-				}
-
-				this.#current = prev;
-			}, 0);
-		}
-	}
-
-	#values = {};
-	#reads = {};
-
-	watch(object, defaults = object, set = () => {}) {
-		for (let [k, initial] of Object.entries(defaults)) {
-			let symbol = Symbol(k);
-
-			this.#reads[symbol] = [];
-
-			Object.defineProperty(object, k, {
-				get: () => {
-					if (Element.#current) {
-						this.#reads[symbol].push(Element.#current);
-					}
-
-					return this.#values[symbol];
-				},
-				set: (v) => {
-					if (this.#values[symbol] !== v) {
-						this.#values[symbol] = v;
-						set?.(k, v);
-						Element.#update([
-							...new Set(this.#reads[symbol]?.splice(0, Infinity)),
-						]);
-					}
-				},
-			});
-
-			object[k] = initial;
-		}
-
-		return object;
-	}
-
 	constructor() {
 		super();
 
-		this.watch(
-			this,
-			this.constructor?.observedAttributeDefaults ?? {},
-			(k, v) => {
-				typeof v === "boolean"
-					? this.toggleAttribute(k, v)
-					: this.setAttribute(k, v);
-			}
-		);
+		watch(this, this.constructor?.observedAttributeDefaults ?? {}, (k, v) => {
+			typeof v === "boolean"
+				? this.toggleAttribute(k, v)
+				: this.setAttribute(k, v);
+		});
 
 		let firstChild = this.firstElementChild;
 		let mode = firstChild?.getAttribute("shadowrootmode");
@@ -88,7 +81,7 @@ export class Element extends HTMLElement {
 	}
 
 	connectedCallback() {
-		Element.#update(this.setupCallback?.() ?? []);
+		update(this.setupCallback?.() ?? []);
 	}
 
 	disconnectedCallback() {
