8450ff5baf5c4a8318beb444dd3749f58cb5d733
erickmerchant <erickmerchant@gmail.com>
2023-10-23 22:34:56 -0400

    Bring back the reactivity to element.js

--- theme/element.js
+++ theme/element.js
@@ -1,3 +1,48 @@
+let current;
+let queue = [];
+
+export let update = (updates) => {
+	let length = queue.length;
+
+	queue.push(...updates);
+
+	if (length === 0) {
+		setTimeout(() => {
+			let prev = current;
+
+			for (let u of queue.splice(0, Infinity)) {
+				current = u;
+				u();
+			}
+
+			current = prev;
+		}, 0);
+	}
+};
+
+export let watch = (object) => {
+	let reads = {};
+
+	for (let key of Object.keys(object)) {
+		reads[key] = [];
+	}
+
+	return new Proxy(object, {
+		get: (target, key, r) => {
+			if (current) {
+				reads[key].push(current);
+			}
+
+			return Reflect.get(target, key, r);
+		},
+		set: (target, key, value, r) => {
+			update([...new Set(reads[key].splice(0, Infinity))]);
+
+			return Reflect.set(target, key, value, r);
+		},
+	});
+};
+
 export class Element extends HTMLElement {
 	constructor() {
 		super();
@@ -9,5 +54,13 @@ export class Element extends HTMLElement {
 			this.attachShadow({mode}).appendChild(firstChild.content.cloneNode(true));
 			firstChild.remove();
 		}
+	}
+
+	connectedCallback() {
+		update(this.setupCallback?.() ?? []);
+	}
+
+	disconnectedCallback() {
+		this.teardownCallback?.();
 	}
 }
