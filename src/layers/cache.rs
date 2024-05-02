2724cf6f6c46c779153f59d0c74cc371d19e37aa
erickmerchant <erickmerchant@gmail.com>
2024-05-01 23:00:37 -0400

    Less format

--- src/layers/cache.rs
+++ src/layers/cache.rs
@@ -10,19 +10,19 @@ use http_body_util::BodyExt;
 use std::fs;
 
 pub async fn layer(req: Request<Body>, next: Next) -> Result<Response, crate::Error> {
-	let (req_parts, req_body) = req.into_parts();
-	let mut cache_path = format!("storage{}", req_parts.uri.path());
+	let mut cache_path = req.uri().path().trim_start_matches('/').to_string();
 
-	if cache_path.ends_with('/') {
+	if cache_path.is_empty() || cache_path.ends_with('/') {
 		cache_path = format!("{cache_path}index.html");
 	}
 
-	let cache_path = Utf8Path::new(&cache_path);
-	let content_type = mime_guess::from_path(cache_path)
+	let cache_path = Utf8Path::new("storage").join(cache_path);
+	let (req_parts, req_body) = req.into_parts();
+	let content_type = mime_guess::from_path(cache_path.clone())
 		.first()
 		.map(|content_type| format!("{}; charset=utf-8", content_type))
 		.unwrap_or("text/html; charset=utf-8".to_string());
-	let body = if let Ok(cached_body) = fs::read_to_string(cache_path) {
+	let body = if let Ok(cached_body) = fs::read_to_string(cache_path.clone()) {
 		cached_body.as_bytes().to_vec()
 	} else {
 		let res = next
