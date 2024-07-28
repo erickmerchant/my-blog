8e525f317a62d7f392c59c1f0df3da83036ac015
erickmerchant <erickmerchant@gmail.com>
2024-07-27 22:30:41 -0400

    Changes

--- src/routes/asset.rs
+++ src/routes/asset.rs
@@ -11,29 +11,30 @@ pub async fn handler(request: Request) -> Result<Response, crate::Error> {
 	let path = request.uri().path().trim_start_matches('/');
 	let path = Utf8Path::new("public").join(path);
 
-	if let Some(ext) = path.extension() {
+	if let Some((cache_control, content_type, body)) = path.clone().extension().and_then(|ext| {
 		let new_path = path.with_extension("").with_extension(ext);
 		let cache_control = if path == new_path {
 			"public, max-age=0, must-revalidate"
 		} else {
 			"public, max-age=31536000, immutable"
 		};
-		let content_type = mime_guess::from_ext(ext).first();
 
-		if let Some(content_type) = content_type {
-			if let Ok(body) = fs::read(new_path) {
-				return Ok((
-					StatusCode::OK,
-					[
-						(header::CONTENT_TYPE, content_type.to_string()),
-						(header::CACHE_CONTROL, cache_control.to_string()),
-					],
-					body,
-				)
-					.into_response());
-			}
-		}
+		mime_guess::from_ext(ext).first().and_then(|content_type| {
+			fs::read(new_path)
+				.ok()
+				.map(|body| (cache_control, content_type, body))
+		})
+	}) {
+		Ok((
+			StatusCode::OK,
+			[
+				(header::CONTENT_TYPE, content_type.to_string()),
+				(header::CACHE_CONTROL, cache_control.to_string()),
+			],
+			body,
+		)
+			.into_response())
+	} else {
+		not_found::handler().await
 	}
-
-	not_found::handler().await
 }
