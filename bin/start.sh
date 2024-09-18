b35fb5bdc674852eaaa8f004f3df53f266ef2847
erickmerchant <erickmerchant@gmail.com>
2024-09-18 16:04:48 -0400

    Better bins

--- bin/start.sh
+++
@@ -1,1 +0,0 @@
-docker run -p 3000:8080 --name my-blog my-blog
--- bin/watch.sh
+++ bin/watch.sh
@@ -1,1 +1,9 @@
-PORT=3000 RUST_LOG=tower_http=trace cargo watch -x 'r --no-default-features'
+trap killgroup SIGINT
+
+killgroup(){
+  kill 0
+}
+
+PORT=3000 RUST_LOG=tower_http=trace cargo watch -x 'r --no-default-features' &
+coolstyleserver proxy public &
+wait
