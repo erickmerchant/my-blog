2abdbac81e414c2cd5e34176f12821764719a0ff
erickmerchant <erickmerchant@gmail.com>
2025-01-16 11:19:16 -0500

    Changes

--- src/routes/asset.rs
+++ src/routes/asset.rs
@@ -28,39 +28,39 @@ pub async fn asset_handler(
 	}
 
 	if let Some(content_type) = mime_guess::from_path(&path).first() {
-		let body = fs::read(Utf8Path::new(&state.base_dir).join(format!("public/{path}")))?;
+		if let Ok(body) = fs::read(Utf8Path::new(&state.base_dir).join(format!("public/{path}"))) {
+			if content_type == mime::TEXT_HTML {
+				let (req_parts, _) = req.into_parts();
+				let headers = &req_parts.headers;
+				let if_none_match = headers.get(header::IF_NONE_MATCH);
+				let uri = &req_parts.uri;
+				let res = (
+					StatusCode::OK,
+					[(header::CONTENT_TYPE, content_type.to_string())],
+					body,
+				)
+					.into_response();
 
-		if content_type == mime::TEXT_HTML {
-			let (req_parts, _) = req.into_parts();
-			let headers = &req_parts.headers;
-			let if_none_match = headers.get(header::IF_NONE_MATCH);
-			let uri = &req_parts.uri;
-			let res = (
+				return apply_html_layer(State(state), if_none_match, uri, res).await;
+			}
+
+			return Ok((
 				StatusCode::OK,
-				[(header::CONTENT_TYPE, content_type.to_string())],
+				[
+					(header::CONTENT_TYPE, content_type.to_string()),
+					(
+						header::CACHE_CONTROL,
+						if query.v.is_some() {
+							"public, max-age=31536000, immutable".to_string()
+						} else {
+							"public, max-age=86400".to_string()
+						},
+					),
+				],
 				body,
 			)
-				.into_response();
-
-			return apply_html_layer(State(state), if_none_match, uri, res).await;
+				.into_response());
 		}
-
-		return Ok((
-			StatusCode::OK,
-			[
-				(header::CONTENT_TYPE, content_type.to_string()),
-				(
-					header::CACHE_CONTROL,
-					if query.v.is_some() {
-						"public, max-age=31536000, immutable".to_string()
-					} else {
-						"public, max-age=86400".to_string()
-					},
-				),
-			],
-			body,
-		)
-			.into_response());
 	}
 
 	not_found::not_found_handler(State(state)).await
