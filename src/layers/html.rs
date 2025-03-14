63db83ace159909835229364a12aa488733f15e7
erickmerchant <erickmerchant@gmail.com>
2025-03-14 12:20:05 -0400

    Preload modules

--- src/layers/html.rs
+++ src/layers/html.rs
@@ -11,9 +11,14 @@ use etag::EntityTag;
 use glob::glob;
 use hyper::{body::Bytes, HeaderMap, Uri};
 use lol_html::{element, html_content::ContentType, text, HtmlRewriter, Settings};
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use serde_json as json;
-use std::{collections::BTreeMap, fs, sync::Arc};
+use std::{
+	collections::{BTreeMap, HashSet},
+	fs,
+	sync::{Arc, LazyLock},
+};
 use url::Url;
 
 pub async fn apply_html_layer(
@@ -30,12 +35,12 @@ pub async fn apply_html_layer(
 
 	let body = res.into_body();
 	let body = to_bytes(body, usize::MAX).await?;
-	let cache_buster = CacheBuster {
+	let optimizer = Optimizer {
 		url: Url::parse("https://0.0.0.0")?.join(uri.to_string().as_str())?,
 		base_dir: state.base_dir.to_owned(),
 	};
 	let body = if state.rewrite_assets {
-		cache_buster.rewrite_assets(&body)?
+		optimizer.rewrite_assets(&body)?
 	} else {
 		body.to_vec()
 	};
@@ -71,48 +76,24 @@ pub async fn html_layer(
 }
 
 type Imports = BTreeMap<String, String>;
-type Scopes = BTreeMap<String, Imports>;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Default, Serialize, Deserialize)]
 pub struct ImportMap {
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub imports: Option<Imports>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub scopes: Option<Scopes>,
+	pub imports: Imports,
 }
 
 impl ImportMap {
-	pub fn map(&mut self, cache_buster: &CacheBuster) -> &Self {
-		self.imports = self.map_imports(self.imports.to_owned(), cache_buster);
-
-		if self.scopes.is_some() {
-			let mut scopes = Scopes::new();
-
-			for (scope_key, old_map) in self.scopes.as_ref().unwrap_or(&Scopes::new()) {
-				if let Some(new_map) = self.map_imports(Some(old_map.to_owned()), cache_buster) {
-					scopes.insert(scope_key.to_owned(), new_map);
-				}
-			}
-
-			self.scopes = Some(scopes);
-		}
-
-		self
-	}
-
-	fn map_imports(&self, imports: Option<Imports>, cache_buster: &CacheBuster) -> Option<Imports> {
-		imports.as_ref()?;
-
+	pub fn map(&mut self, optimizer: &Optimizer) -> &Self {
 		let mut new_imports = Imports::new();
 
-		for (key, value) in imports.as_ref().unwrap_or(&Imports::new()) {
+		for (key, value) in self.imports.clone() {
 			if value.ends_with("/") {
-				if let Ok(full_url) = cache_buster.url.join(value) {
+				if let Ok(full_url) = optimizer.url.join(&value) {
 					let full_url_path = full_url.path();
 					let results = glob(
 						format!(
 							"{}/public/{}**/*.js",
-							cache_buster.base_dir,
+							optimizer.base_dir,
 							full_url_path.trim_start_matches("/")
 						)
 						.as_str(),
@@ -123,13 +104,12 @@ impl ImportMap {
 						for path in paths.flatten() {
 							let path = path.to_string_lossy();
 							let relative_key = path.trim_start_matches(
-								format!("{}/public", cache_buster.base_dir)
-									.trim_start_matches("./"),
+								format!("{}/public", optimizer.base_dir).trim_start_matches("./"),
 							);
 							let relative_path = path.trim_start_matches(
 								format!(
 									"{}/public/{}",
-									cache_buster.base_dir,
+									optimizer.base_dir,
 									full_url_path.trim_start_matches("/")
 								)
 								.trim_start_matches("./"),
@@ -137,11 +117,11 @@ impl ImportMap {
 
 							new_imports.insert(
 								format!("{key}{relative_path}"),
-								cache_buster.get_url(format!("{value}{relative_path}").as_str()),
+								optimizer.get_url(format!("{value}{relative_path}").as_str(), true),
 							);
 							new_imports.insert(
 								relative_key.to_string(),
-								cache_buster.get_url(format!("{value}{relative_path}").as_str()),
+								optimizer.get_url(format!("{value}{relative_path}").as_str(), true),
 							);
 						}
 					}
@@ -149,22 +129,57 @@ impl ImportMap {
 
 				new_imports.insert(key.to_owned(), value.to_owned());
 			} else {
-				new_imports.insert(key.to_owned(), cache_buster.get_url(value));
+				new_imports.insert(key.to_owned(), optimizer.get_url(&value, true));
 			}
 		}
 
-		Some(new_imports)
+		self.imports = new_imports;
+
+		self
+	}
+
+	fn resolve(&self, specifier: String, base: &str) -> String {
+		let mut specifier = specifier;
+
+		if !specifier.starts_with("./")
+			&& !specifier.starts_with("../")
+			&& !specifier.starts_with("/")
+		{
+			for (key, path) in self.imports.clone() {
+				if specifier.starts_with(&key) {
+					let new_specifier = specifier.trim_start_matches(&key);
+
+					specifier = path + new_specifier;
+
+					break;
+				}
+			}
+		}
+
+		let base = Url::parse("https://0.0.0.0")
+			.expect("should be a url")
+			.join(base)
+			.expect("should be a url");
+		let specifier = base.join(specifier.as_str()).expect("should be a url");
+		let specifier = specifier.path();
+
+		specifier.to_string()
 	}
 }
 
-pub struct CacheBuster {
+pub struct Optimizer {
 	base_dir: String,
 	url: Url,
 }
 
-impl CacheBuster {
+const PRELOAD_MODULES_COMMENT: &str = "<!-- preloadmodules -->";
+
+impl Optimizer {
 	pub fn rewrite_assets(&self, bytes: &Bytes) -> anyhow::Result<Vec<u8>> {
 		let mut output = Vec::new();
+		let mut import_map = ImportMap::default();
+		let mut modules = Vec::new();
+		let mut module_blocks = Vec::new();
 		let mut rewriter = HtmlRewriter::new(
 			Settings {
 				element_content_handlers: [
@@ -180,8 +195,11 @@ impl CacheBuster {
 						"link[href][rel='stylesheet'], link[href][rel='icon']",
 						|el| {
 							if let Some(href) = el.get_attribute("href") {
-								el.set_attribute("href", self.get_url(href.as_str()).as_str())
-									.ok();
+								el.set_attribute(
+									"href",
+									self.get_url(href.as_str(), true).as_str(),
+								)
+								.ok();
 							}
 
 							Ok(())
@@ -189,20 +207,41 @@ impl CacheBuster {
 					),
 					element!("script[src]", |el| {
 						if let Some(src) = el.get_attribute("src") {
-							el.set_attribute("src", self.get_url(src.as_str()).as_str())
-								.ok();
+							let url = self.get_url(src.as_str(), false);
+
+							if let Some(t) = el.get_attribute("type") {
+								if t == "module" {
+									modules.push(url.clone());
+								}
+							}
+
+							let url = self.get_url(src.as_str(), true);
+
+							el.set_attribute("src", url.as_str()).ok();
 						}
 
 						Ok(())
 					}),
-					text!("script[type='importmap']", |el| {
-						if let Ok(mut import_map) = json::from_str::<ImportMap>(el.as_str()) {
-							import_map.map(self);
+					text!("script[type='module']", |el| {
+						module_blocks.push(el.as_str().to_string());
 
-							if let Ok(map) = json::to_string(&import_map) {
+						Ok(())
+					}),
+					text!("script[type='importmap']", |el| {
+						if let Ok(mut im) = json::from_str::<ImportMap>(el.as_str()) {
+							import_map = im.clone();
+
+							im.map(self);
+
+							if let Ok(map) = json::to_string(&im) {
 								el.replace(map.as_str(), ContentType::Text);
 							}
 						}
+
+						Ok(())
+					}),
+					element!("script[type='importmap']", |el| {
+						el.after(PRELOAD_MODULES_COMMENT, ContentType::Html);
 
 						Ok(())
 					}),
@@ -216,10 +255,76 @@ impl CacheBuster {
 		rewriter.write(bytes.as_ref())?;
 		rewriter.end()?;
 
-		Ok(output)
+		let mut preload_modules: HashSet<String> = HashSet::new();
+
+		for module in modules.clone() {
+			preload_modules.insert(module);
+		}
+
+		for block in module_blocks.clone() {
+			preload_modules = self.get_deps(
+				self.url.to_string(),
+				Some(block),
+				preload_modules.clone(),
+				import_map.clone(),
+			);
+		}
+
+		for module in modules.clone() {
+			preload_modules = self.get_deps(module, None, preload_modules, import_map.clone());
+		}
+
+		let mut preloads = String::new();
+
+		for preload_module in preload_modules {
+			let preload_module = self.get_url(preload_module.as_str(), true);
+
+			preloads.push_str(
+				format!(r#"<link rel="modulepreload" href="{}" />"#, preload_module).as_str(),
+			);
+		}
+
+		let output = String::from_utf8(output.to_vec()).expect("should be a string");
+		let output = output.replace(PRELOAD_MODULES_COMMENT, preloads.as_str());
+
+		Ok(output.into())
 	}
 
-	pub fn get_url(&self, url: &str) -> String {
+	pub fn get_deps(
+		&self,
+		url: String,
+		body: Option<String>,
+		found: HashSet<String>,
+		import_map: ImportMap,
+	) -> HashSet<String> {
+		static IMPORT_REGEX: LazyLock<Regex> =
+			LazyLock::new(|| Regex::new(r#"\s*import.*?('(.*?)'|"(.*?)")"#).unwrap());
+
+		let mut results = found.clone();
+		let body = body.unwrap_or_else(|| {
+			let file_path = format!("{}/public/{}", self.base_dir, url.trim_start_matches("/"));
+
+			fs::read_to_string(Utf8Path::new(file_path.as_str()).to_path_buf()).unwrap_or_default()
+		});
+
+		for (_, [_, a]) in IMPORT_REGEX.captures_iter(&body).map(|c| c.extract()) {
+			let resolved = import_map.resolve(a.to_string(), &url);
+
+			if !results.contains(&resolved) {
+				results.insert(resolved.clone());
+
+				for r in self.get_deps(resolved, None, found.clone(), import_map.clone()) {
+					if !results.contains(&r) {
+						results.insert(r);
+					}
+				}
+			}
+		}
+
+		results
+	}
+
+	pub fn get_url(&self, url: &str, with_hash: bool) -> String {
 		if let Ok(full_url) = self.url.join(url) {
 			let full_url_path = full_url.path();
 			let file_path = format!(
@@ -228,19 +333,25 @@ impl CacheBuster {
 				full_url_path.trim_start_matches("/")
 			);
 
-			if let Ok(body) = fs::read_to_string(Utf8Path::new(file_path.as_str()).to_path_buf()) {
-				let hash = md5::compute(body.as_bytes());
-				let mut new_ext = format!("{hash:?}")[0..10].to_string();
-				let path = Utf8Path::new(full_url_path);
+			if with_hash {
+				if let Ok(body) =
+					fs::read_to_string(Utf8Path::new(file_path.as_str()).to_path_buf())
+				{
+					let hash = md5::compute(body.as_bytes());
+					let mut new_ext = format!("{hash:?}")[0..10].to_string();
+					let path = Utf8Path::new(full_url_path);
 
-				if let Some(ext) = path.extension() {
-					new_ext = format!("{new_ext}.{ext}");
+					if let Some(ext) = path.extension() {
+						new_ext = format!("{new_ext}.{ext}");
+					}
+
+					let path = path.with_extension(new_ext);
+
+					return path.to_string();
 				}
-
-				let path = path.with_extension(new_ext);
-
-				return path.to_string();
 			}
+
+			return full_url_path.to_string();
 		};
 
 		url.to_string()
