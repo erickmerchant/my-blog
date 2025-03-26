4b0da46e674f06d5b8c7a8bd734a8545cf1efc47
erickmerchant <erickmerchant@gmail.com>
2025-03-26 00:58:32 -0400

    Remove cache busting stuff from page handler

--- src/routes/page.rs
+++ src/routes/page.rs
@@ -20,37 +20,13 @@ pub async fn page_handler(
 	} else {
 		path.to_path_buf()
 	};
-	let mut has_hash = false;
-	let mut path = path.to_path_buf();
-	let file_name_parts: Vec<&str> = path
-		.file_name()
-		.map(|name| name.split('.').collect::<_>())
-		.unwrap_or_default();
-
-	if file_name_parts.len() == 3 && file_name_parts[1].len() == 10 {
-		has_hash = true;
-
-		path = path
-			.with_file_name(file_name_parts[0])
-			.with_extension(file_name_parts[2]);
-	}
 
 	if let Ok(body) =
 		fs::read(Utf8Path::new(&state.base_dir).join("public/".to_string() + path.as_str()))
 	{
 		return Ok((
 			StatusCode::OK,
-			[
-				(header::CONTENT_TYPE, mime::HTML.to_string()),
-				(
-					header::CACHE_CONTROL,
-					if has_hash {
-						"public, max-age=31536000, immutable".to_string()
-					} else {
-						"public, max-age=86400".to_string()
-					},
-				),
-			],
+			[(header::CONTENT_TYPE, mime::HTML.to_string())],
 			body,
 		)
 			.into_response());
