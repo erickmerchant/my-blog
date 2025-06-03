a1bb6f3755a408140f23fe60ae7fd5aec1c14adc
erickmerchant <erickmerchant@gmail.com>
2025-06-03 11:49:43 -0400

    Finalize rendering refactor

--- app/html.ts
+++ app/html.ts
@@ -105,7 +105,7 @@ export async function optimizeHTML(path: string) {
     }
 
     if (inlineCSS) {
-      const style = doc.createElement("style");
+      const style = doc.createElement("style", {});
 
       style.textContent = inlineCSS;
 
