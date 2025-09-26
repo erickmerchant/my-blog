b430d5e372ea8ec10bba37755eaabe058de5bafb
erickmerchant <erickmerchant@gmail.com>
2025-09-26 01:54:16 -0400

    Update

--- flint.ts
+++ flint.ts
@@ -3,8 +3,8 @@ import flint, { pattern as p } from "@flint/framework";
 import { getPostURLs as posts } from "./src/models/post.ts";
 import home from "./src/views/home.ts";
 import post from "./src/views/post.ts";
-import rss from "./src/views/rss.ts";
 import resume from "./src/views/resume.ts";
+import rss from "./src/views/rss.ts";
 import notFound from "./src/views/404.ts";
 
 const app = flint("src", "dist")
