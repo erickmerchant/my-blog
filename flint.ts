77742874ba0fbda5e1233ffd798ffa5f486a3cc6
erickmerchant <erickmerchant@gmail.com>
2025-11-04 01:07:14 -0500

    Move views to pages

--- flint.ts
+++ flint.ts
@@ -1,11 +1,11 @@
 import css from "@flint/framework/handlers/css";
 import flint, { pattern as p } from "@flint/framework";
 import { getPostURLs as posts } from "./models/post.ts";
-import home from "./views/home.ts";
-import post from "./views/post.ts";
-import resume from "./views/resume.ts";
-import rss from "./views/rss.ts";
-import notFound from "./views/404.ts";
+import home from "./pages/home.ts";
+import post from "./pages/post.ts";
+import resume from "./pages/resume.ts";
+import rss from "./pages/rss.ts";
+import notFound from "./pages/404.ts";
 
 const app = flint()
   .route("/", home)
