bf82bf25bf85284498223b65a73fb06e557dc77c
erickmerchant <erickmerchant@gmail.com>
2025-11-09 00:47:31 -0500

    cli changes. updates

--- cli/post-draft.ts
+++ cli/post-draft.ts
@@ -2,17 +2,40 @@ import * as Fs from "@std/fs";
 import * as Path from "@std/path";
 import * as Toml from "@std/toml";
 import { slugify } from "@std/text/unstable-slugify";
+import { parseArgs } from "@std/cli/parse-args";
+
+const helpText = `
+create a new post in draft state (no datepublished)
+
+--title   the title of the post. it will be slugified
+`;
 
 if (import.meta.main) {
-  const title = Deno.args.join("");
-  const slug = slugify(title);
-  const frontmatter = Toml.stringify({
-    title,
+  const flags = parseArgs(Deno.args, {
+    boolean: ["help"],
+    string: ["title"],
+    alias: {
+      h: "help",
+    },
   });
-  const code = `+++\n${frontmatter}+++\n`;
-  const path = `content/posts/${slug}.md`;
 
-  await Fs.ensureDir(Path.dirname(path));
+  if (!flags.help && flags.title != null && flags.title != "") {
+    const title = flags.title;
+    const slug = slugify(title);
+    const frontmatter = Toml.stringify({
+      title,
+    });
+    const code = `+++\n${frontmatter}+++\n`;
+    const path = `content/posts/${slug}.md`;
 
-  await Deno.writeTextFile(path, code);
+    await Fs.ensureDir(Path.dirname(path));
+
+    await Deno.writeTextFile(path, code);
+
+    Deno.exit(0);
+  } else {
+    console.log(helpText);
+
+    Deno.exit(1);
+  }
 }
--- cli/post-publish.ts
+++ cli/post-publish.ts
@@ -2,21 +2,45 @@ import * as Fs from "@std/fs";
 import * as Path from "@std/path";
 import * as Toml from "@std/toml";
 
+import { parseArgs } from "@std/cli/parse-args";
+
+const helpText = `
+publish a post (set its datepublished)
+
+--path   the path to the post relative to the cwd
+`;
+
 if (import.meta.main) {
-  const path = Deno.args[0];
-  const text = await Deno.readTextFile(path);
-  const [, toml, md] = text.split("+++\n");
-  const parsed: {
-    title?: string;
-    datePublished?: string;
-  } = Toml.parse(toml);
+  const flags = parseArgs(Deno.args, {
+    boolean: ["help"],
+    string: ["path"],
+    alias: {
+      h: "help",
+    },
+  });
 
-  parsed.datePublished = Temporal.Now.plainDateISO().toString();
+  if (!flags.help && flags.path != null && flags.path != "") {
+    const path = flags.path;
+    const text = await Deno.readTextFile(path);
+    const [, toml, md] = text.split("+++\n");
+    const parsed: {
+      title?: string;
+      datePublished?: string;
+    } = Toml.parse(toml);
 
-  const frontmatter = Toml.stringify(parsed);
-  const code = `+++\n${frontmatter}+++\n${md}`;
+    parsed.datePublished = Temporal.Now.plainDateISO().toString();
 
-  await Fs.ensureDir(Path.dirname(path));
+    const frontmatter = Toml.stringify(parsed);
+    const code = `+++\n${frontmatter}+++\n${md}`;
 
-  await Deno.writeTextFile(path, code);
+    await Fs.ensureDir(Path.dirname(path));
+
+    await Deno.writeTextFile(path, code);
+
+    Deno.exit(0);
+  } else {
+    console.log(helpText);
+
+    Deno.exit(1);
+  }
 }
