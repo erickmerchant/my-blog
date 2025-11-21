2dac2ec437a9515cc7af8285919b99a759d1462a
erickmerchant <erickmerchant@gmail.com>
2025-11-21 09:18:15 -0500

    Fix highlighting in worker

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -1,19 +1,20 @@
 import MarkdownIt from "markdown-it";
-import prism from "markdown-it-prism";
-
-import "prismjs/components/prism-bash.js";
-import "prismjs/components/prism-clike.js";
-import "prismjs/components/prism-markup.js";
-import "prismjs/components/prism-css.js";
-import "prismjs/components/prism-javascript.js";
-import "prismjs/components/prism-typescript.js";
-import "prismjs/components/prism-docker.js";
-import "prismjs/components/prism-rust.js";
-
-const md = new MarkdownIt();
-
-md.use(prism);
 
 export async function parse(markdown: string): Promise<string> {
-  return await md.render(markdown);
+  const { default: prism } = await import("markdown-it-prism");
+
+  await import("prismjs/components/prism-clike.js");
+  await import("prismjs/components/prism-markup.js");
+  await import("prismjs/components/prism-css.js");
+  await import("prismjs/components/prism-javascript.js");
+  await import("prismjs/components/prism-typescript.js");
+  await import("prismjs/components/prism-rust.js");
+  await import("prismjs/components/prism-docker.js");
+  await import("prismjs/components/prism-bash.js");
+
+  const md = new MarkdownIt();
+
+  md.use(prism, {});
+
+  return md.render(markdown);
 }
