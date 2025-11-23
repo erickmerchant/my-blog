dc342b935e956d62231493e3274310a185fe48f2
erickmerchant <erickmerchant@gmail.com>
2025-11-23 01:56:20 -0500

    Changes

--- pages/page.ts
+++ pages/page.ts
@@ -48,25 +48,22 @@ export default function (
   );
   const baseFooter = footer.class("footer")(
     ul.class("footer-list")(
-      li.class("footer-item")(
-        a.class("footer-link rss-link").href("/posts.rss")(
-          span.class("icon")(
-            svg.viewBox("0 0 28 28")(
-              svgTitle("RSS Feed"),
-              path.d(
-                "M0 24 A4 4 0 1 1 8 24 A4 4 0 1 1 0 24 Z M0 11 Q0 10 1 10 Q18 10 18 27 Q18 28 17 28 H13 Q12 28 12 27 Q12 16 1 16 Q0 16 0 15 Z M0 1 Q0 0 1 0 Q28 0 28 27 Q28 28 27 28 H23 Q22 28 22 27 Q22 6 1 6 Q0 6 0 5 Z",
-              ),
-            ),
-          ),
-          "RSS",
-        ),
-      ),
+      li.class("footer-item")("© " + site.author),
       li.class("footer-item")(
         a.class("footer-link").href("https://github.com/erickmerchant/my-blog")(
           "View Source",
         ),
       ),
-      li.class("footer-item")("© " + site.author),
+      li.class("footer-item")(
+        a.class("footer-link rss-link").href("/posts.rss")(
+          svg.viewBox("0 0 28 28")(
+            svgTitle("RSS Feed"),
+            path.d(
+              "M0 24 A4 4 0 1 1 8 24 A4 4 0 1 1 0 24 Z M0 11 Q0 10 1 10 Q18 10 18 27 Q18 28 17 28 H13 Q12 28 12 27 Q12 16 1 16 Q0 16 0 15 Z M0 1 Q0 0 1 0 Q28 0 28 27 Q28 28 27 28 H23 Q22 28 22 27 Q22 6 1 6 Q0 6 0 5 Z",
+            ),
+          ),
+        ),
+      ),
     ),
   );
   const baseBody = body.class("page")(baseNav, ...main, baseFooter);
