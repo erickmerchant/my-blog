23b67df42e2790a0fa9f221158f29b7a2359b066
erickmerchant <erickmerchant@gmail.com>
2026-07-20 15:42:53 -0700

    Use @erickmerchant/icons

--- pages/page.ts
+++ pages/page.ts
@@ -1,6 +1,7 @@
 import { type Site } from "../models/site.ts";
 import { h, type HandcraftChild, type HandcraftNode } from "@handcraft/lib";
 import favicons from "./favicons.ts";
+import { icon as rssIcon } from "@erickmerchant/icons/rss";
 
 type Config = {
   site: Site;
@@ -25,7 +26,6 @@ const {
   html,
   main,
 } = h.html;
-const { title: svgTitle, path, svg } = h.svg;
 
 export default function (
   {
@@ -71,14 +71,7 @@ export default function (
       li.class("item")(
         a
           .class("link rss")
-          .href("/posts.rss")(
-            svg.viewBox("0 0 28 28")(
-              svgTitle("RSS Feed"),
-              path.d(
-                "M0 24 A4 4 0 1 1 8 24 A4 4 0 1 1 0 24 Z M0 11 Q0 10 1 10 Q18 10 18 27 Q18 28 17 28 H13 Q12 28 12 27 Q12 16 1 16 Q0 16 0 15 Z M0 1 Q0 0 1 0 Q28 0 28 27 Q28 28 27 28 H23 Q22 28 22 27 Q22 6 1 6 Q0 6 0 5 Z",
-              ),
-            ),
-          ),
+          .href("/posts.rss")(rssIcon()),
       ),
     ),
   );
